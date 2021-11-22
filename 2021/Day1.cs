5768a56802eecae38ad704c54d951d3f5d362018
Joseph Kingry <joe@kingry.ca>
2021-11-22 07:08:34 -0500

    2021 template

---
+++ 2021/Day1.cs
@@ -0,0 +1,18 @@
+namespace AdventOfCode2021;
+
+class Day1 : IRobotElf
+{
+    public int Part1(string? path = null)
+    {
+        path = path ?? "input/1.txt";
+
+        return -1;
+    }
+
+    public int Part2(string? path = null) 
+    {
+        path = path ?? "input/1.txt";
+
+        return -1;
+    }
+}
---
+++ 2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode2021;
+
+
+var days = new IRobotElf[] {
+            new Day1(),
+        };
+
+var day = args.Length > 0 && int.TryParse(args[0], out var temp) ? temp : days.Length;
+day--;
+
+Console.WriteLine($"Part1: {days[day].Part1()}");
+Console.WriteLine($"Part2: {days[day].Part2()}");
