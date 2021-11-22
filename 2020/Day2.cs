500d5c2e5c14fdb3dd043c7d63c098d540c5f85f
Joseph Kingry <joe@kingry.ca>
2021-11-22 06:45:16 -0500

    day2 part2

--- 2020/Day2.cs
+++ 2020/Day2.cs
@@ -36,6 +36,29 @@ class Day2 : IRobotElf
 
     public int Part2(string? path = null) 
     {
-        return -1;
+        path = path ?? "input/2.txt";
+
+        bool checkValid(string line)
+        {
+            var m = Regex.Match(line, @"([0-9]+)\-([0-9]+) ([a-z])\: ([a-z]+)");
+            if (!m.Success)
+            {
+                throw new InvalidOperationException($"Line doesn't match: {line}");
+            }
+
+            var p1 = int.Parse(m.Groups[1].Value) - 1;
+            var p2 = int.Parse(m.Groups[2].Value) - 1;
+            var c = m.Groups[3].Value[0];
+            var password = m.Groups[4].Value;
+
+            return password[p1] == c ^ password[p2] == c;
+        }
+
+        var numbers =
+            from line in File.ReadLines(path)
+            where checkValid(line)
+            select line;
+
+        return numbers.Count();
     }
 }
