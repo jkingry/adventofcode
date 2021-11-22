7b317787192d2fc31b8882619af6e460ec664ed2
Joseph Kingry <joe@kingry.ca>
2021-11-22 07:27:38 -0500

    Day4

---
+++ 2020/Day6.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode2020;
+
+class Day6 : RobotElf
+{
+    public Day6() : base(6) {}
+
+    public override int Part1()
+    {
+        return -1;
+    }
+
+    public override int Part2()
+    {
+        return -1;
+    }
+}
