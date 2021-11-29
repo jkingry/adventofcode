e8e5ca4191243fbb8dcd8207007dfdee2bc9ebdc
Joseph Kingry <joe@kingry.ca>
2021-11-28 21:46:49 -0500

    2019 day 1 fsharp and csharp

---
+++ 2019/csharp/IRobotElf.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode2019;
+
+using System.Linq;
+
+interface IRobotElf 
+{
+    long Part1();
+    long Part2();
+}
+
+abstract class RobotElf : IRobotElf
+{
+    protected RobotElf(int day) {
+        Day = day;
+    }
+
+    public int Day { get; }
+
+    protected IEnumerable<string> Input => File.ReadLines($"../input/{Day}.txt");
+
+    protected IEnumerable<int> InputNumbers => File.ReadLines($"../input/{Day}.txt").Select(int.Parse);
+
+    public abstract long Part1();
+
+    public abstract long Part2();
+} 
---
+++ 2019/fsharp/day1.fs
@@ -0,0 +1,19 @@
+namespace AdventOfCode2019
+
+open System
+
+module Day1 =
+
+    let part1 (input : string seq) =
+        let f x = max ((x / 3) - 2) 0
+        let q = input |> Seq.map Int32.Parse |> Seq.map f
+        q |> Seq.sum
+
+    let part2 (input : string seq) = 
+        let f x  = max ((x / 3) - 2) 0
+        let g x =
+            let y = f x 
+            if y > 0 then Some(y, y) else None
+        let h x = (List.unfold g x) |> List.sum
+
+        input |> Seq.map Int32.Parse |> Seq.map h |> Seq.sum
