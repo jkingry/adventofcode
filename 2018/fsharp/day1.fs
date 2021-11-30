e7737541044f4abcfb408ce1b49046b02028f404
Joseph Kingry <joe@kingry.ca>
2021-11-29 22:22:38 -0500

    cleaned up

--- 2018/fsharp/day1.fs
+++ 2018/fsharp/day1.fs
@@ -14,14 +14,28 @@ module Day1 =
 
     let part2 (input : string seq)  = 
         let x = parse input
-
-        let mutable notFound : int option = None
-        let mutable s = Set.empty
+        
+        let mutable dupe : int option = None
+        let mutable found = Set.empty
         let mutable sum = 0
-        while notFound.IsNone do
+        while dupe.IsNone do
             for y in x do
                 sum <- sum + y
-                if Set.contains sum s then
-                    notFound <- Some sum
-                s <- Set.add sum s
-        notFound.Value
+                if Set.contains sum found then
+                    dupe <- Some sum
+                found <- Set.add sum found
+        dupe.Value
+
+
+    let part3 (input : string seq)  = 
+        let x = parse input
+
+        let rec f i sum found =
+            let ni = i % x.Length
+            let nsum = x[ni] + sum
+            if Set.contains nsum found then
+                nsum
+            else            
+                f (ni + 1) nsum (Set.add nsum found)
+
+        f 0 0 Set.empty
