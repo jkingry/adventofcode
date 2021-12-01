3bf6b5219e83adca067cf4899a5bee5e05007af5
Joseph Kingry <joe@kingry.ca>
2021-12-01 01:27:50 -0500

    final refactor day 1

--- 2021/fsharp/Day01.fs
+++ 2021/fsharp/Day01.fs
@@ -3,16 +3,20 @@ namespace AdventOfCode.FSharp.Y2021
 open System
 
 module Day01 =
+    let increasing input =
+        input
+        |> Seq.pairwise
+        |> Seq.filter (fun (a,b) -> b > a)
+        |> Seq.length
+
     let part1 input =
         input 
         |> Seq.map Int32.Parse
-        |> Seq.pairwise
-        |> Seq.fold (fun t (a,b) -> t + if b > a then 1 else 0) 0
+        |> increasing
 
     let part2 input = 
         input
         |> Seq.map Int32.Parse
         |> Seq.windowed 3
         |> Seq.map Seq.sum
-        |> Seq.pairwise
-        |> Seq.fold (fun t (a,b) -> t + if b > a then 1 else 0) 0
+        |> increasing
