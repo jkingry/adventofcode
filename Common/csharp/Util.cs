580a8d48a21c87372ea8b83285c44825d2d33dd0
Joseph Kingry <joe@kingry.ca>
2021-12-01 08:07:17 -0500

    window using array segments

--- Common/csharp/Util.cs
+++ Common/csharp/Util.cs
@@ -45,4 +45,15 @@ public static class Util
 
         return a | b;
     }
+
+    public static IEnumerable<ArraySegment<T>> Window<T>(this T[] input, int window)
+    {
+        ArraySegment<T> root = input;
+
+        for (var i = 0; i < input.Length; ++i)
+        {
+            if (i + window <= input.Length)
+                yield return root.Slice(i, window);
+        }
+    }    
 }
