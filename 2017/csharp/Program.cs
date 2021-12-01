80f4e1aaaac615df91306cdee7e927eab0ce1b2b
Joseph Kingry <joe@kingry.ca>
2021-11-30 21:53:56 -0500

    2017 day 1 csharp

---
+++ 2017/csharp/Program.cs
@@ -0,0 +1,5 @@
+global using System.Text;
+global using System.Text.RegularExpressions;
+global using static System.Math;
+
+AdventOfCode.CSharp.Util.Run();
---
+++ 2017/fsharp/Program.fs
@@ -0,0 +1,1 @@
+AdventOfCode.FSharp.Util.runCommandLine ()
