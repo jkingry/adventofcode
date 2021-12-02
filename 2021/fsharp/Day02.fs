510ef836ca80b4de32606917c596a9f595da6e28
Joseph Kingry <joe@kingry.ca>
2021-12-02 00:13:21 -0500

    rough work used to solve 2021 day 2

--- 2021/fsharp/Day02.fs
+++ 2021/fsharp/Day02.fs
@@ -1,6 +1,9 @@
 namespace AdventOfCode.FSharp.Y2021
 
 open System
+open System.Text
+open System.Text.RegularExpressions
+open AdventOfCode.FSharp.Util
 
 module Day02 =
     let increasing input =
@@ -9,10 +12,26 @@ module Day02 =
         |> Seq.filter (fun (a,b) -> (b > a))
         |> Seq.length
 
-    let part1 input =
+    let part1 (input : string seq)=        
+        let mutable depth = 0
+        let mutable h = 0
+        let mutable aim = 0
+
         input 
-        |> Seq.map Int32.Parse
-        |> increasing
+        |> Seq.iter (fun line -> 
+            let p = line.Split(' ')
+            let d = Int32.Parse(p[1])
+            match p[0] with 
+            | "down" ->
+                 aim <- aim + d
+            | "up" ->
+                aim <- aim - d
+
+            | "forward" -> 
+                h <- h + d
+                depth <- depth + (aim * d))
+
+        h * depth
 
     let part2 input = 
         input
