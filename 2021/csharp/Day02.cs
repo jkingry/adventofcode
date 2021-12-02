5188a00a5e2256409b45bd0f2515263e2904ecd6
Joseph Kingry <joe@kingry.ca>
2021-12-01 21:54:02 -0500

    2021 day 2 prep

---
+++ 2021/csharp/Day02.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode.CSharp.Y2021;
+class Day02 : AdventOfCode.CSharp.RobotElf
+{
+    public Day02() : base(2) {}
+
+    public override object Part1()
+    {
+        var numbers = Input.Select(int.Parse);
+
+        return Increasing(numbers);
+    }
+
+    public override object Part2() 
+    {
+        var windows = Input
+            .Select(int.Parse)
+            .ToArray()
+            .Window(3)
+            .Select(s => s.Sum());
+
+        return Increasing(windows);
+    }
+
+    int Increasing(IEnumerable<int> numbers)
+    {
+        var last = int.MaxValue;
+        var total = 0;
+        foreach (var n in numbers)
+        {
+            if (n > last) total += 1;
+            last = n;
+        }
+        return total;
+    }
+}
