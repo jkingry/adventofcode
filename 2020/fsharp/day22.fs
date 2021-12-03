3fd48394014d4481f6a0e1ea30c683eda44b2990
Joseph Kingry <joe@kingry.ca>
2021-12-02 21:32:51 -0500

    2020 22 done

--- 2020/fsharp/day22.fs
+++ 2020/fsharp/day22.fs
@@ -8,9 +8,11 @@ open FParsec
 open System.Collections.Generic
 open AdventOfCode.FSharp.Util
 
+type Decks = Map<int, int list>
+
 // Crab Combat
 module Day22 =
-    let parse (input : string seq) =
+    let parse (input : string seq) : Decks =
         let parsePlayer (e: IEnumerator<string>) =
             if e.MoveNext() = false then None else
 
@@ -22,31 +24,56 @@ module Day22 =
         let e = input.GetEnumerator ()
         Seq.unfold parsePlayer e |> Map.ofSeq
 
-
-    let round (mp : Map<int, int list>) =
-        let nmp = mp |> Map.map (fun _ v -> v.Tail)
-
-        let winner = if mp[1].Head > mp[2].Head then 1 else 2
-        let loser = if winner = 1 then 2 else 1
-        let winnings = [ mp[winner].Head ; mp[loser].Head ]
-
-        let fmp = nmp |> Map.add winner (nmp[winner] @ winnings)
-
-        let res = if fmp[loser].Length = 0 then Some winner else None
-        (res, fmp)
-
-
-    let part1 (input : string seq) =
-        let mutable mp = parse input
+    let combatGame (round : Decks -> int) (mp : Decks) : int * int list =
+        let mutable state = mp
         
-        let mutable winner = None
+        let mutable gameWinner = None
 
-        while Option.isNone winner do 
-            let results = round mp
-            winner <- fst results
-            mp <- snd results
+        let mutable pastStates = Set.empty
 
-        let d = mp[winner.Value]
+        while Option.isNone gameWinner do 
+            if pastStates |> Set.contains state then gameWinner <- Some 1 else
+            pastStates <- Set.add state pastStates
 
-        let n = d.Length
-        d |> List.mapi (fun i v -> (n - i) * v ) |> List.sum
+            let winner = round state
+            let loser = if winner = 1 then 2 else 1
+
+            let tails = state |> Map.map (fun _ v -> v.Tail)
+            let heads = state |> Map.map (fun _ v -> v.Head)
+
+            let winnings = [ heads[winner] ; heads[loser] ]
+            state <- tails |> Map.add winner (tails[winner] @ winnings)
+
+            if state[loser].Length = 0 then gameWinner <- Some winner
+
+        (gameWinner.Value, state[gameWinner.Value])
+        
+    let combatRound (mp : Decks) =
+        if mp[1].Head > mp[2].Head then 1 else 2
+        
+    let part1 (input : string seq) =
+        let mp = parse input
+        
+        let (_, winnerDeck) = mp |> combatGame combatRound
+
+        let n = winnerDeck.Length
+        winnerDeck |> List.mapi (fun i v -> (n - i) * v ) |> List.sum
+
+    let rec recCombatRound (mp : Decks) =
+        let tails = mp |> Map.map (fun _ v -> v.Tail)
+        let heads = mp |> Map.map (fun _ v -> v.Head)
+        
+        if Seq.forall2 (fun h t -> h <= List.length t) heads.Values tails.Values then
+            let recDeck = tails |> Map.map (fun p v -> v |> List.take heads[p])
+            recDeck |> combatGame recCombatRound |> fst
+        else
+            if heads[1] > heads[2] then 1 else 2
+    
+    let part2 (input : string seq) =
+        let mp = parse input
+        
+        let (_, winnerDeck) = mp |> combatGame recCombatRound
+
+        let n = winnerDeck.Length
+        winnerDeck |> List.mapi (fun i v -> (n - i) * v ) |> List.sum
+
