93b44e55c4d15dd090021ae9b0ceefff1bc3ee1c
Joseph Kingry <joe@kingry.ca>
2021-12-03 23:11:22 -0500

    2021 day 3 fsharp clean

--- 2021/fsharp/Day03.fs
+++ 2021/fsharp/Day03.fs
@@ -1,34 +1,54 @@
-namespace AdventOfCode.FSharp.Y2021
+namespace AdventOfCode.FSharp.Y2021.Day03
 
 open System
 open System.Text
 open System.Text.RegularExpressions
 open AdventOfCode.FSharp.Util
 
-type Pos = 
-    {
-        depth : int
-        h : int
-        aim : int
-    }
-
 module Day03 =
-    let executeInput exec (input : string seq) =
-        let { depth = depth; h = h } = 
-            input
-            |> Seq.map (fun line -> let p = line.Split(' ') in (p[0], Int32.Parse p[1]))
-            |> Seq.fold exec { depth = 0; h = 0; aim = 0 }
-        depth * h    
+    let fromBinary (s : string) = 
+        let mutable n = 0
+        for i = 0 to s.Length - 1 do
+            if s[s.Length - i - 1] = '1' then n <- n + (1 <<< i)
+        n
 
-    let part1 (input : string seq) =
-        let n = 5
-        let a = Array.zeroCreate n
-        let counts =
-            input
-            |> Seq.iter (fun s -> 
-                s |> Seq.iteri (fun i c -> if c = '1' then a[i] <- a[i] + 1)) 
+    let part1 (input : string seq) =        
+        let mapIncr (i : int) (m : Map<int, int>) = m |> Map.change i (fun x -> Some (1 + Option.defaultValue 0 x))
         
-        Array.mapi (fun i aa -> if  )
-        -1
-    let part2 input = 
-        -1
+        let countOnes (s : string) (counts : Map<int, int>) =
+            s |> Seq.indexed |> Seq.fold (fun a (i, c) -> if c = '1' then (mapIncr i a) else a) counts
+
+        let cache = input |> Seq.toList
+
+        let counts = 
+            cache
+            |> Seq.fold (fun m s -> m |> countOnes s) Map.empty
+
+        let n = cache.Length
+        let gammaChars =
+            counts 
+            |> Map.map (fun _ v -> if v > (n/2) then '1' else '0') 
+            |> Map.toSeq |> Seq.sortBy fst |> Seq.map snd |> Seq.toArray
+
+        let gamma = new string(gammaChars) |> fromBinary 
+
+        let digits = (List.head cache).Length
+        let mask = (1 <<< digits) - 1
+        let epsilon = (~~~gamma) &&& mask
+
+        epsilon * gamma
+
+    let part2 (input : string seq) : int =        
+        let filterReport (dir: bool) (input : string list)  (p : int) = 
+            let m = input |> List.groupBy (fun s -> s[p]) |> Map.ofList
+            if (m['0'].Length > m['1'].Length) = dir then m['0'] else m['1']
+
+        let foldReport (input : string list) (dir : bool) =
+            let n = (List.head input).Length
+            [0..n-1] |> Seq.scan (filterReport dir) input |> Seq.find (fun l -> l.Length = 1)
+            |> List.head |> fromBinary     
+
+        let cache = input |> List.ofSeq
+        let oxygenGenerator = foldReport cache true
+        let co2Scrubber  = foldReport cache false
+        oxygenGenerator * co2Scrubber
