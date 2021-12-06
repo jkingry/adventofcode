17a529a2069608f1703b6c29e0e12450e216d212
Joseph Kingry <joe@kingry.ca>
2021-12-06 00:34:28 -0500

    2021 6 rough submit

--- 2021/fsharp/Day06.fs
+++ 2021/fsharp/Day06.fs
@@ -31,9 +31,38 @@ module Day06 =
         |> Seq.length
 
     let part1 (input : string) =
-        let lines = input |> lineSplit |> Seq.map parseLine
+        // let mutable n = ints(input) |> Array.toList
+
+
+        // for d=1 to 80 do
+        //     let mutable nn : int list = []
+        //     for i=0 to n.Length-1 do
+        //         let nnn =
+        //             match n[i] with 
+        //             | 0 -> nn <- 8::nn ; 6
+        //             | _ -> n[i] - 1
+        //         nn <- nnn::nn
+        //     n <- nn
+        // Seq.length n
         -1
+
+
  
 
     let part2 (input : string) =
-        -1
+        let mutable m  = ints(input) |> Array.map (fun v -> int64 v) |> Array.toList |> List.groupBy id |> List.map (fun (k,v) -> (k,List.length v |> int64) ) |> Map.ofList
+
+
+
+        for d=1 to 256 do
+            let mutable mm : Map<int64, int64> = Map.empty
+            for x in m do
+                let k : int64 = x.Key
+                let v : int64 = x.Value
+                if k = 0 then   
+                    mm <- mm |> Map.change 8 (fun vv -> match vv with | Some n -> Some (n + v) | _ -> Some v)         
+                    mm <- mm |> Map.change 6 (fun vv -> match vv with | Some n -> Some (n + v) | _ -> Some v)            
+                else
+                    mm <- mm |> Map.change (k - 1L) (fun vv -> match vv with | Some n -> Some (n + v) | _ -> Some v)            
+            m <- mm
+        m.Values |> Seq.sum
