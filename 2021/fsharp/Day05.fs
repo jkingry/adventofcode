4b3d06b6bfeb04101853fcf6b6103b71130dda4c
Joseph Kingry <joe@kingry.ca>
2021-12-05 20:21:14 -0500

    2021 day 5 clean

--- 2021/fsharp/Day05.fs
+++ 2021/fsharp/Day05.fs
@@ -1,43 +1,50 @@
 namespace AdventOfCode.FSharp.Y2021
 
 open System
+open AdventOfCode.FSharp.Util
 
 // Day 5: 
 module Day05 =    
-    let ints (s : string) =
-        s.Split(' ',',') 
-        |> Array.filter (not << String.IsNullOrWhiteSpace)
-        |> Array.map (fun s -> s.Trim() |> Int32.Parse) 
+    let parseLine text =
+        match text with
+        | Regex @"(\d+),(\d+) -> (\d+),(\d+)" [x1;y1;x2;y2] -> 
+            ([Int32.Parse x1;Int32.Parse y1], [Int32.Parse x2;Int32.Parse y2])
+        | _ -> failwith "Invalid"
         
+    let points (p1, p2) =
+        let delta = List.map2 (fun a b -> sign(b - a)) p1 p2
+        let mutable n = p1
+        seq {
+            yield n
+            while n <> p2 do 
+                n <- List.map2 (fun a b -> a + b) n delta
+                yield n
+        }
+
+    let incr v =
+        match v with 
+        | Some s -> Some (s + 1)
+        | _ -> Some 1
 
     let part1 (input : string seq) =
-        let n = 
-            input
-            |> Seq.map (fun s -> 
-                let p = s.Split(' ')
-                (ints(p[0]) , ints(p[p.Length - 1])))
-            |> Seq.toList
+        let lines = input |> Seq.map parseLine
+ 
+        let marked =
+            lines
+            |> Seq.filter (fun (p1, p2) -> p1[0] = p2[0] || p1[1] = p2[1]) 
+            |> Seq.map points 
+            |> Seq.concat
+            |> Seq.fold (fun m s -> m |> Map.change s incr) Map.empty
 
-        printfn "%A" n
-        let mutable m = Map.empty
-        for p in n do
-            let (x1,x2) = p
-            let p1 = x1
-            let p2 = x2
-            let d = [ 
-                sign (p2[0] - p1[0])
-                sign (p2[1] - p1[1])
-            ]
-            let mutable x = p1[0]
-            let mutable y = p1[1]
-            let mutable going = true
-            while going do
-                m <- m |> Map.change (x,y) (function | None -> Some 1 | Some x -> Some (x + 1))
-                if x = p2[0] && y = p2[1] then going <- false
-                x <- x + d[0]
-                y <- y + d[1]
-    
-        m.Values |> Seq.filter (fun v -> v > 1) |> Seq.length        
+        marked.Values |> Seq.filter (fun v -> v > 1) |> Seq.length
 
-    let part2 (input : string) =
-        -1    
+    let part2 (input : string seq) =
+        let lines = input |> Seq.map parseLine
+ 
+        let marked =
+            lines
+            |> Seq.map points 
+            |> Seq.concat
+            |> Seq.fold (fun m s -> m |> Map.change s incr) Map.empty
+
+        marked.Values |> Seq.filter (fun v -> v > 1) |> Seq.length
