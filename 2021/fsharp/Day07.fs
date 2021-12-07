67c85ade0e84f5bca72ae7c79d0fbf41a50a573a
Joseph Kingry <joe@kingry.ca>
2021-12-07 00:19:01 -0500

    2021 day 7 rough solution

---
+++ 2021/fsharp/Day07.fs
@@ -0,0 +1,34 @@
+namespace AdventOfCode.FSharp.Y2021
+
+open AdventOfCode.FSharp.Util
+open System
+// Day 7
+module Day07 =    
+
+    let part1 (text : string) =
+        let input = 
+            text |> ints |> Array.groupBy id 
+            |> Array.map (fun (k,v) -> (k, Array.length v))
+            |> Map.ofArray
+        
+        let total = input |> Map.fold (fun a k v -> a + (k * v) ) 0
+        let avg = total / (input.Values |> Seq.sum)
+
+        let imin = input.Keys |> Seq.min
+        let imax = input.Keys |> Seq.max
+
+        printfn "min %d" imin
+        printfn "max %d" imax
+
+        let sint n = n * (n+1) / 2
+        let cost x q =
+            q |> Map.fold (fun a k v -> a + sint(abs(k - x)) * v) 0
+
+        let (sol, solcost) = [imin..imax] |> List.fold (fun (a,c) i ->
+            let cc = cost i input
+            if cc < c then (i,cc) else (a,c)) (-1, Int32.MaxValue)
+
+        solcost
+
+    let part2 (input : string) =
+        -1
