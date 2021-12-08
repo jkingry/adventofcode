68d2d0e30786f76ed45a07bc621e1f3ea33a334b
Joseph Kingry <joe@kingry.ca>
2021-12-07 23:56:10 -0500

    2021 day 8 prep

---
+++ 2021/fsharp/Day08.fs
@@ -0,0 +1,14 @@
+namespace AdventOfCode.FSharp.Y2021
+
+// Day 8
+module Day08 =    
+    open AdventOfCode.FSharp.Util
+    open Checked
+
+    let part1 (text : string) =
+        let input = text |> ints 
+        -1
+
+    let part2 (text : string) =
+        let input = text |> ints 
+        -1
