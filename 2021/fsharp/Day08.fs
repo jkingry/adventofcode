490dafa91ef1e765bb551d48958a63cb977ad4e3
Joseph Kingry <joe@kingry.ca>
2021-12-08 22:42:28 -0500

    optimal

--- 2021/fsharp/Day08.fs
+++ 2021/fsharp/Day08.fs
@@ -3,60 +3,16 @@ namespace AdventOfCode.FSharp.Y2021
 // Day 8
 module Day08 =    
     open AdventOfCode.FSharp.Util
-    open Checked
 
-    let charMap = 
-        [
-            "abcefg"
-            "cf"
-            "acdeg"
-            "acdfg"
-            "bcdf"
-            "abdfg"
-            "abdefg"
-            "acf"
-            "abcdefg"
-            "abcdfg"
-        ] |> List.indexed |> List.map (fun (a,b) -> (b,a.ToString()[0])) |> Map.ofList
-
-    let charArrayToString (a : char[]) = new System.String(a)
-
-    let fastPermute (e : string seq) =
-        let one = e |> Seq.find (fun s -> s |> String.length = 2) |> List.ofSeq
-        let seven = e |> Seq.find (fun s -> s |> String.length = 3) |> List.ofSeq
-        let four = e |> Seq.find (fun s -> s |> String.length = 4) |> List.ofSeq
-        let all = "abcdefg" |> List.ofSeq
-        let input : char list list =
-            [
-                seven|> List.except one  // a
-                four |> List.except one  // b
-                one                      // c
-                four |> List.except one  // d
-                all  |> List.except four // e
-                one                      // f
-                all  |> List.except four // g 
-            ]
-
-        input
-        |> fromChoices
-        |> List.map (fun l -> l |> List.toArray |> charArrayToString)
+    let (&&&) (a : Set<'a>) (b : Set<'a>) =
+        Set.intersect a b
 
     let parse (input : string) =
         input
         |> splitLine
         |> Array.map (fun line -> 
             let parts = line |> split "|" |> Array.map splitSpace
-            parts[0], parts[1])
-    
-    let translate cmap input =
-        let segments : string =
-                input
-                |> Seq.map (fun c -> Map.find c cmap)
-                |> Seq.sort
-                |> Seq.toArray
-                |> charArrayToString
-            
-        Map.tryFind segments charMap      
+            parts[0], parts[1])   
 
     let part1 (text : string) =
         let uniqueNumbers = set [ 2; 3; 4; 7 ]
@@ -67,30 +23,29 @@ module Day08 =
         |> Array.map Array.length
         |> Array.sum
 
+    let solve (x : string seq, o : string seq) =
+        let l = seq { for s in x -> s.Length, set s } |> Map.ofSeq
+
+        let mutable n = ""
+
+        for s in o |> Seq.map set do
+            let c = 
+                match s.Count, (s &&& l[4]).Count, (s &&& l[2]).Count with
+                | 2,_,_ -> "1"
+                | 3,_,_ -> "7"
+                | 4,_,_ -> "4"
+                | 7,_,_ -> "8"
+                | 5,2,_ -> "2"
+                | 5,3,1 -> "5"
+                | 5,3,2 -> "3"
+                | 6,3,1 -> "6"
+                | 6,4,2 -> "9"
+                | 6,3,2 -> "0"
+                | _ -> failwith "Invalid"
+            n <- n + c
+        int n 
+
     let part2 (text : string) =
-        printfn "%A" (fook |> Seq.toList)
-
-        let stringToMap s =
-            Seq.zip s "abcdefg" |> Map.ofSeq
-        let success e cmap =
-            e |> Seq.forall ((translate cmap) >> Option.isSome)
-
-        let mutable p = 0
-        let res = 
-            parse text
-            |> Array.map (fun (e,o) ->
-                let cmap : Map<char,char> = 
-                    fastPermute e
-                    |> List.map (fun s -> p <- p + 1; s)                
-                    |> List.map stringToMap 
-                    |> Seq.find (success e)
-
-                o |> Seq.map (fun oo -> 
-                    (oo |> translate cmap).Value) 
-                    |> Seq.toArray 
-                    |> charArrayToString 
-                    |> int)
-            |> Array.sum
-        
-        printfn "%i" p
-        res
+        parse text
+        |> Array.map solve
+        |> Array.sum
