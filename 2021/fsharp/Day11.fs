45a71cb4c8c89ed96b3747a40fe467ba09082238
Joseph Kingry <joe@kingry.ca>
2021-12-11 01:27:06 -0500

    2021 11 cleaner

--- 2021/fsharp/Day11.fs
+++ 2021/fsharp/Day11.fs
@@ -1,33 +1,37 @@
 namespace AdventOfCode.FSharp.Y2021
 
-// Day 11
+// Day 11: Dumbo Octopus
 module Day11 =    
     open AdventOfCode.FSharp.Util
     open Checked
 
-    let rec flash x y (grid : int[,])  =
-        for nx = x-1 to x+1 do
-            for ny = y-1 to y+1 do
-                if (nx <> x || ny <> y) &&  nx >= 0 && ny >= 0 && nx < Array2D.length1 grid && ny < Array2D.length2 grid then
-                    let v = grid[nx,ny] + 1
-                    grid[nx,ny] <- v
-
     let step grid = 
-        let flashed = ref Set.empty
         grid  |> Array2D.iteri (fun x y v -> grid[x,y] <- v + 1)
         
-        let mutable found = true
+        let mutable flashed = Set.empty
 
-        while found do
-            found <- false
-            grid |> Array2D.iteri (fun x y v -> 
-                if v > 9 && not (Set.contains (x, y) flashed.Value) then
-                    found <- true
-                    flashed.Value <- Set.add (x,y) flashed.Value
-                    flash x y grid)
+        let flash (x,y) =
+            for nx = x-1 to x+1 do
+                for ny = y-1 to y+1 do
+                    if (nx <> x || ny <> y) &&  nx >= 0 && ny >= 0 && nx < Array2D.length1 grid && ny < Array2D.length2 grid then
+                        grid[nx,ny] <- grid[nx,ny] + 1
 
-        !flashed |> Seq.iter (fun (x,y) -> grid[x,y] <- 0)
-        flashed.Value.Count        
+        let findDumbos g =
+            let mutable f = Set.empty
+            g |> Array2D.iteri (fun x y v -> 
+                if v > 9 && (flashed |> Set.contains (x,y) |> not) then f <- f |> Set.add (x,y))
+            f
+
+        let mutable dumbos = findDumbos grid
+
+        while dumbos.Count > 0 do
+            flashed <- flashed |> Set.union dumbos     
+            dumbos |> Seq.iter flash
+            dumbos <- findDumbos grid
+
+        flashed |> Seq.iter (fun (x,y) -> grid[x,y] <- 0)
+
+        flashed.Count    
 
     let part1 (text : string) =   
         let grid = 
@@ -36,14 +40,16 @@ module Day11 =
             |> Array.map (fun line -> line |> Seq.map (string >> int))
             |> array2D
 
-        let mutable ff = 0
-        for i = 1 to 100000 do
-            let f = step grid
-            if f = 100 then 
-                printfn "FOUND IT %d" i
-                ignore <| System.Console.ReadLine ()
-            ff <- ff + f
-        ff
+        [1..100] |> List.map(fun _ -> step grid) |> List.sum
 
     let part2 (text : string) =
-        -1
+        let grid = 
+            text 
+            |> splitLine
+            |> Array.map (fun line -> line |> Seq.map (string >> int))
+            |> array2D
+
+        let n = grid.Length
+
+        Seq.initInfinite (fun i -> i, step grid) 
+        |> Seq.pick (fun (i, v) -> if v = n then Some (i+1) else None)
