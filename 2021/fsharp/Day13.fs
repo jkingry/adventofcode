e25ee06ef5aec990e5e7ca7e8a9b9d07b9399e39
Joseph Kingry <joe@kingry.ca>
2021-12-13 01:08:26 -0500

    gradual testing

--- 2021/fsharp/Day13.fs
+++ 2021/fsharp/Day13.fs
@@ -1,18 +1,16 @@
 namespace AdventOfCode.FSharp.Y2021
 
-// Day 13
+// Day 13: Transparent Origami
 module Day13 =     
     open AdventOfCode.FSharp.Util
 
-    let part1 (text : string) : string =
-        let mutable dots =
+    let parse text =
+        let dots =
             (text |> splitDoubleLine)[0] 
             |> splitLine
             |> Array.map ints
             |> Array.map (fun [|x;y|] -> (x,y))
-            |> Set.ofArray
-        
-        
+            |> Set.ofArray                
         let folds =
             (text |> splitDoubleLine)[1]
             |> splitLine
@@ -20,37 +18,48 @@ module Day13 =
                 match line with
                 | Regex @"fold along (y|x)=(\d+)" [axis;num] -> (axis, int num)
                 | _ -> failwith "Invalid")
+        dots,folds
 
-        let foldy (yy:int) (i : Set<int*int>) : Set<int*int> = 
-            let bottom = i |> Seq.filter(fun (_,y) -> y > yy)
-            bottom 
-            |> Seq.fold (fun a (x,y) -> a |> Set.add (x, yy - (y - yy))) i
-            |> Set.filter (fun (_,y) -> y <= yy)
+    let foldy fold dots  = 
+        dots 
+        |> Seq.filter(fun (_,y) -> y > fold)
+        |> Seq.fold (fun a (x,y) -> a |> Set.add (x, 2 * fold - y)) dots
+        |> Set.filter (fun (_,y) -> y <= fold)
 
-        let foldx (yy:int) (i : Set<int*int>) : Set<int*int> = 
-            let bottom = i |> Seq.filter(fun (y,_) -> y > yy)
-            bottom 
-            |> Seq.fold (fun a (y,x) -> a |> Set.add (yy - (y - yy), x)) i
-            |> Set.filter (fun (y,_) -> y <= yy)            
-        
-        for f in folds do
-            match f with
-            | ("x",n) -> dots <- foldx n dots
-            | ("y",n) -> dots <- foldy n dots
+    let foldx fold dots  = 
+        dots 
+        |> Seq.filter(fun (x,_) -> x > fold)
+        |> Seq.fold (fun a (x,y) -> a |> Set.add (2 * fold - x, y)) dots
+        |> Set.filter (fun (x,_) -> x <= fold)      
+    
+    let fold dots f =
+        match f with
+        | ("x",n) -> foldx n dots
+        | ("y",n) -> foldy n dots
+        | _ -> failwith "unreachable"
 
-        let maxx = dots |> Seq.map (fun (x,_) -> x) |> Seq.max
-        let maxy = dots |> Seq.map (fun (_,y) -> y) |> Seq.max
+    let part1 text =
+        let (dots, folds) = parse text
+
+        let firstFold = folds |> Array.head
+        fold dots firstFold |> Set.count |> string
+
+    let part2 text =   
+        let (dots, folds) = parse text
+
+        let foldedDots =
+            folds
+            |> Array.fold fold dots
+
+        let maxx = foldedDots |> Seq.map (fun (x,_) -> x) |> Seq.max
+        let maxy = foldedDots |> Seq.map (fun (_,y) -> y) |> Seq.max
 
         let mutable result = System.Environment.NewLine
 
         for y=0 to maxy do
             let s = new System.Text.StringBuilder (String.replicate (maxx + 1) ".")
-            let row = dots |> Seq.filter (fun (_,yy) -> yy = y)
+            let row = foldedDots |> Seq.filter (fun (_,yy) -> yy = y)
             for (x,_) in row do
                 s[x] <- '#'
-            result <- result + (string s) + System.Environment.NewLine
-  
+            result <- result + (string s) + System.Environment.NewLine  
         result
-
-    let part2 (text : string) : string =   
-        -1 |> string
