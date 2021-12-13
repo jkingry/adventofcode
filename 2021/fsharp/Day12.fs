d0f2d24161c4f33c14459111aa82a48228b90b27
Joseph Kingry <joe@kingry.ca>
2021-12-12 20:19:07 -0500

    temp

--- 2021/fsharp/Day12.fs
+++ 2021/fsharp/Day12.fs
@@ -1,62 +1,92 @@
 namespace AdventOfCode.FSharp.Y2021
 
 // Day 12
-module Day12 =    
+module Day12 =     
     open AdventOfCode.FSharp.Util
     open Checked
 
-    let part1 (text : string) =   
-        let caves : Map<string, Set<string>> = 
+    type graph<'k when 'k: comparison> = Map<'k, Set<'k>>
+
+    module Graph =
+        let empty : graph<'k> = Map.empty
+        let add (source: 'k) (destination: 'k) (g: graph<'k>): graph<'k> =
+            g |> Map.change source (fun v -> 
+                (match v with | Some s -> s | _ -> Set.empty) |> Set.add destination |> Some)
+        let fromList (input: ('k*'k) list) : graph<'k> =
+            input
+            |> List.fold (fun g (src,dest) -> g |> add src dest) empty
+        let filterEdges (fn: 'k -> bool) (graph: graph<'k>): graph<'k> =
+            graph |> Map.map (fun _ v -> v |> Set.filter fn)
+        let filterNodes (fn: 'k -> bool) (graph: graph<'k>): graph<'k> =
+            graph |> Map.filter (fun k _ -> fn k)
+        let link (g: graph<'k>): graph<'k> =
+            g
+            |> Map.toSeq
+            |> Seq.collect (fun (k,v) -> v |> Seq.map (fun e -> k ,e))
+            |> Seq.fold (fun g' (src, dest) -> add dest src g') g
+
+
+    let parse text =
+        let pairs = 
             text 
             |> splitLine 
-            |> Array.fold (fun c line ->
-                let p = line.Split('-')
-                let a = p[0]
-                let b = p[1]
-                let ca = 
-                    if a <> "end" && b <> "start"then
-                        c |> Map.change a (fun o -> (defaultArg o Set.empty) |> Set.add b |> Some) 
-                    else 
-                        c
-                if b <> "end" && a <> "start" then
-                    ca |> Map.change b (fun o -> (defaultArg o Set.empty) |> Set.add a |> Some)
-                else
-                    ca) Map.empty 
-                
-        printfn "%A" caves
+            |> Array.map (fun line -> let p = line.Split('-') in p[0],p[1])
+            |> Array.toList
+        
+        (pairs @ (List.map (fun (a,b) -> (b,a)) pairs))
+        |> List.filter (fun (src,dest) -> src <> "end" && dest <> "start")
+        |> List.fold (fun g (src,dest) -> 
+            g |> Map.change src (fun v -> 
+                (match v with | Some s -> s | _ -> Set.empty) |> Set.add dest |> Some))  Map.empty
 
-        let small (k : string) = 
-            k.ToLowerInvariant() = k
-
-        let mutable q = [(["start"], Set.empty, None)]
-
+    let findPaths visitRule (caves : Map<string, Set<string>>) =
+           
+        let mutable q = [(["start"], Set.empty, false)]
         let mutable ends = []
 
         while q.Length > 0 do    
-            let (path, visited, dcave)::xs = q
-            
-            q <- xs
-            
-            let pathNode = List.head path
+            match q with
+            | (current::path, visited, doubleCave)::next ->                            
+                q <- next
+                if current = "end" then ends <- path::ends else
+                q <- 
+                    caves[current] 
+                    |> Seq.choose (visitRule path visited doubleCave)
+                    |> Seq.fold (fun a x -> x::a) q
+            | _ -> failwith "Unreachable"
+        ends
 
-            if pathNode = "end" then
-                ends <- path::ends
+    let inline small (k: string) = k.ToLower() = k 
+
+    let part1 (text : string) =   
+        let caves = parse text
+
+        let visitRule path visited doubleCave k =
+            if not (small k) then
+                Some (k::path, visited, doubleCave)
             else
-                for nn in caves[pathNode] do
-                    if (small nn) then
-                        if visited |> Set.contains nn then 
-                            match dcave with
-                            | Some _ -> ()
-                            | None -> q <- (nn::path, visited, Some nn)::q
-                        else
-                            q <- (nn::path, visited |> Set.add nn, dcave)::q
-                    else
-                        q <- (nn::path, visited, dcave)::q 
+                if not (Set.contains k visited) then
+                    Some (k::path, visited, doubleCave)
+                else    
+                    None   
 
-            //printfn "%A" (q.Length, ends.Length)
-        printfn "%A" (ends |> (List.map List.rev) |> List.sort)
+        let paths = caves |> findPaths visitRule                        
 
-        ends.Length
+        List.length paths
 
-    let part2 (text : string) =
-        -1
+    let part2 (text : string) =   
+        let caves = parse text
+
+        let visitRule path visited doubleCave k =
+            if not (small k) then
+                Some (k::path, visited, doubleCave)
+            else
+                if not (Set.contains k visited) then
+                    Some (k::path, visited |> Set.add k, doubleCave)
+                elif not doubleCave then
+                    Some (k::path, visited, true)
+                else
+                    None   
+
+        let paths = caves |> findPaths visitRule
+        List.length paths
