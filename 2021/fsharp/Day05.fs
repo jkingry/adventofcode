ee070245198be210e0ff6cd4ae8a085d238718ee
Joseph Kingry <joe@kingry.ca>
2021-12-13 07:04:15 -0500

    2021 day 5 - faster using 2D array to mark points

--- 2021/fsharp/Day05.fs
+++ 2021/fsharp/Day05.fs
@@ -6,34 +6,38 @@ module Day05 =
     
     let parseLine text =
         match text with
-        | Regex @"(\d+),(\d+) -> (\d+),(\d+)" [x1;y1;x2;y2] -> 
-            ([int x1;int y1], [int x2;int y2])
+        | Regex @"(\d+),(\d+) -> (\d+),(\d+)" [x1;y1;x2;y2] -> (int x1,int y1), (int x2,int y2)
         | _ -> failwith "Invalid"
         
-    let points (p1, p2) =
-        let delta = List.map2 (fun a b -> sign(b - a)) p1 p2
-        let mutable n = p1
-        seq {
-            yield n
-            while n <> p2 do 
-                n <- List.map2 (fun a b -> a + b) n delta
-                yield n
-        }
+    let points ((x1,y1), (x2,y2)) (grid : int[,])  =
+        let (dx, dy) = (sign (x2 - x1), sign (y2 - y1))
+        let mutable (nx, ny) = (x1, y1)
+        let mutable doublePoints = 0
+
+        let inline mark x y =
+            let v = grid[x,y]
+            if v = 1 then
+                doublePoints <- doublePoints + 1        
+            grid[x,y] <- v + 1
+
+        mark nx ny
+        while (nx, ny) <> (x2, y2) do 
+            nx <- nx + dx
+            ny <- ny + dy
+            mark nx ny
+        doublePoints
 
     let countPoints lines =
-        lines
-        |> Seq.map points
-        |> Seq.concat
-        |> Seq.groupBy id
-        |> Seq.map snd
-        |> Seq.filter (fun v -> (Seq.length v) > 1)
-        |> Seq.length
+        let (mx,my) = lines |> Array.fold (fun (mx,my) ((x1,y1), (x2,y2)) -> (max (max mx x1) x2, max (max my y1) y2)) (0, 0)
+        let grid = Array2D.zeroCreate (mx+1) (my+1)
+
+        lines |> Array.fold (fun a line -> a + (points line grid)) 0
 
     let part1 (input : string) =
         let lines = input |> splitLine |> Array.map parseLine
  
         lines
-        |> Seq.filter (fun (p1, p2) -> p1[0] = p2[0] || p1[1] = p2[1]) 
+        |> Array.filter (fun ((x1,y1), (x2,y2)) -> x1 = x2 || y1 = y2) 
         |> countPoints
         |> string
 
