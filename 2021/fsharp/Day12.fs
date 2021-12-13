06d62c556c0de91add8d19ed1246d8fedb52181f
Joseph Kingry <joe@kingry.ca>
2021-12-12 21:02:37 -0500

    2021 day 12 clean, added Graph module

--- 2021/fsharp/Day12.fs
+++ 2021/fsharp/Day12.fs
@@ -1,9 +1,8 @@
 namespace AdventOfCode.FSharp.Y2021
 
-// Day 12
+// Day 12: Passage Pathing
 module Day12 =     
     open AdventOfCode.FSharp.Util
-    open Checked
 
     type graph<'k when 'k: comparison> = Map<'k, Set<'k>>
 
@@ -24,69 +23,46 @@ module Day12 =
             |> Map.toSeq
             |> Seq.collect (fun (k,v) -> v |> Seq.map (fun e -> k ,e))
             |> Seq.fold (fun g' (src, dest) -> add dest src g') g
+        let rec findPaths (path: 'k list) (dest: 'k) (visitor: 'State -> 'k -> 'State option) (state: 'State) (g: graph<'k>): 'k list seq =
+            seq {
+                match path with
+                | x::_ when x = dest -> yield path
+                | x::_ ->
+                    for n in g[x] do
+                        match visitor state n with
+                        | Some nextState ->     
+                            yield! findPaths (n::path) dest visitor nextState g
+                        | _ -> ()
+                | _ -> failwith "unreachable"
+            }            
 
-
-    let parse text =
-        let pairs = 
-            text 
-            |> splitLine 
-            |> Array.map (fun line -> let p = line.Split('-') in p[0],p[1])
-            |> Array.toList
-        
-        (pairs @ (List.map (fun (a,b) -> (b,a)) pairs))
-        |> List.filter (fun (src,dest) -> src <> "end" && dest <> "start")
-        |> List.fold (fun g (src,dest) -> 
-            g |> Map.change src (fun v -> 
-                (match v with | Some s -> s | _ -> Set.empty) |> Set.add dest |> Some))  Map.empty
-
-    let findPaths visitRule (caves : Map<string, Set<string>>) =
-           
-        let mutable q = [(["start"], Set.empty, false)]
-        let mutable ends = []
-
-        while q.Length > 0 do    
-            match q with
-            | (current::path, visited, doubleCave)::next ->                            
-                q <- next
-                if current = "end" then ends <- path::ends else
-                q <- 
-                    caves[current] 
-                    |> Seq.choose (visitRule path visited doubleCave)
-                    |> Seq.fold (fun a x -> x::a) q
-            | _ -> failwith "Unreachable"
-        ends
+    let parse text : graph<string> =
+        text 
+        |> splitLine 
+        |> Array.map (fun line -> let p = line.Split('-') in p[0],p[1])
+        |> Array.toList
+        |> Graph.fromList
+        |> Graph.link
+        |> Graph.filterEdges (fun e -> e <> "start")
+        |> Graph.filterNodes (fun n -> n <> "end")
 
     let inline small (k: string) = k.ToLower() = k 
 
+    let part1Visitor visited node =
+        if not (small node) then Some visited 
+        elif visited |> Set.contains node then None
+        else Some (visited |> Set.add node)
+
     let part1 (text : string) =   
         let caves = parse text
+        caves |> Graph.findPaths ["start"] "end" part1Visitor Set.empty |> Seq.length
 
-        let visitRule path visited doubleCave k =
-            if not (small k) then
-                Some (k::path, visited, doubleCave)
-            else
-                if not (Set.contains k visited) then
-                    Some (k::path, visited, doubleCave)
-                else    
-                    None   
-
-        let paths = caves |> findPaths visitRule                        
-
-        List.length paths
+    let part2Visitor (visited, doubleCave) node =
+        match part1Visitor visited node, doubleCave with
+        | Some state,_ -> Some (state, doubleCave)
+        | None, false -> Some (visited, true)
+        | _ -> None
 
     let part2 (text : string) =   
         let caves = parse text
-
-        let visitRule path visited doubleCave k =
-            if not (small k) then
-                Some (k::path, visited, doubleCave)
-            else
-                if not (Set.contains k visited) then
-                    Some (k::path, visited |> Set.add k, doubleCave)
-                elif not doubleCave then
-                    Some (k::path, visited, true)
-                else
-                    None   
-
-        let paths = caves |> findPaths visitRule
-        List.length paths
+        caves |> Graph.findPaths ["start"] "end" part2Visitor (Set.empty, false) |> Seq.length
