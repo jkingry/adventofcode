0e30a84ee474fd5d24539e444ded09a974fa81da
Joseph Kingry <joe@kingry.ca>
2021-12-14 07:59:37 -0500

    2021 14 - clean up, optimization?

--- 2021/fsharp/Day14.fs
+++ 2021/fsharp/Day14.fs
@@ -3,51 +3,60 @@ namespace AdventOfCode.FSharp.Y2021
 // Day 14: Extended Polymerization
 module Day14 =
     open AdventOfCode.FSharp.Util
-    open Checked
+
     let parse text =
         let sections = text |> splitDoubleLine
+
         let parseRule (line: string) =
             let p = line.Split("->")
-            let ab = p[0].Trim() 
-            let c = p[1].Trim()[0]
-            let a = ab[0]
-            let b = ab[1]
-            ab, (c, sprintf "%c%c" a c, sprintf "%c%c" c b) 
+            let ab = p.[0].Trim()
+            let c = p.[1].Trim().[0]
+            let a = ab.[0]
+            let b = ab.[1]
+            ab, (c, sprintf "%c%c" a c, sprintf "%c%c" c b)
 
-        let rules = 
+        let rules =
             sections.[1]
             |> splitLine
             |> Array.map parseRule
             |> Map.ofArray
 
-        (rules, sections[0])
+        (rules, sections.[0])
 
     let pairCounts text =
-        text 
+        text
         |> Seq.pairwise
-        |> Seq.map (fun (a,b) -> sprintf "%c%c" a b)
+        |> Seq.map (fun (a, b) -> sprintf "%c%c" a b)
         |> Counter.create
 
-    let react rules (polymer, atoms) =
-        polymer |> Map.fold (fun (poly',atoms') ab count ->
-            match rules |> Map.tryFind ab with
-            | Some (c,ac,cb) -> 
-                poly' |> Counter.remove ab count |> Counter.add ac count |> Counter.add cb count,
-                atoms' |> Counter.add c count
-            | _ -> poly',atoms') (polymer,atoms)
+    let inline react rules (polymer, atoms) =
+        polymer
+        |> Map.fold
+            (fun (poly', atoms') ab count ->
+                match rules |> Map.tryFind ab with
+                | Some (c, ac, cb) ->
+                    poly'
+                    |> Counter.remove ab count
+                    |> Counter.add ac count
+                    |> Counter.add cb count,
+                    atoms' |> Counter.add c count
+                | _ -> poly', atoms')
+            (polymer, atoms)
 
     let run reactor polymerString n =
         let mutable polymer = pairCounts polymerString
         let mutable atoms = polymerString |> Counter.create
-        for i=1 to n do
-            let (npolymer,natoms) = (polymer, atoms) |> reactor
+
+        for _ = 1 to n do
+            let (npolymer, natoms) = (polymer, atoms) |> reactor
             polymer <- npolymer
             atoms <- natoms
-        let weights = atoms |> Map.toList |> List.map snd |> List.sort
-        
-        let least = weights |> List.head
-        let most = weights |> List.last        
-        most - least           
+
+        let (most, least) =
+            atoms.Values
+            |> Seq.fold (fun (mv, lv) v -> (max v mv), (min v lv)) (System.Int64.MinValue, System.Int64.MaxValue)
+
+        most - least
 
     let part1 (text: string) =
         let (rules, polymerString) = parse text
