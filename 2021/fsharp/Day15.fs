ac7d37594e4dea76a6fb092f143e16ec9d6eb397
Joseph Kingry <joe@kingry.ca>
2021-12-15 08:24:56 -0500

    2021 day 15 clean..er

--- 2021/fsharp/Day15.fs
+++ 2021/fsharp/Day15.fs
@@ -12,44 +12,44 @@ module Day15 =
         if y < (my - 1) then n <- (x,y+1)::n
         n
 
-    let reconstruct_path (cameFrom: ((int*int) option)[,]) (cx,cy) =
-        let mutable total_path = [(cx,cy)]
+    let reconstructPath (cameFrom: ((int*int) option)[,]) (cx,cy) =
+        let mutable path = [(cx,cy)]
         let mutable x = cx
         let mutable y = cy
-        while Option.isSome cameFrom.[x,y] do
-            let (nx,ny) = (cameFrom.[x,y]).Value
-            x <- nx
-            y <- ny
-            total_path <- (x,y)::total_path
-        total_path
+        let mutable finished = false
+        while not finished do
+            match cameFrom.[x,y] with
+            | Some (nx, ny) ->
+                path <- (nx,ny)::path
+                x <- nx
+                y <- ny
+            | _ -> finished <- true
+        path
 
     open Checked 
     open FSharpx.Collections
 
     let astar (sx,sy) (gx,gy) (mx,my) (w: int->int->int) =
-        printfn "START"
         let inline h x y = (gx - x) + (gy - y)
-        let cameFrom = Array2D.create mx my None
 
+        let cameFrom = Array2D.create mx my None
         let gScore = Array2D.create mx my System.Int32.MaxValue
         gScore[sx,sy] <- 0
 
-        let hs = (h sx sy)
-
-        let mutable openSet = Heap.empty false |> Heap.insert (hs, sx, sy)
+        let mutable openSet = Heap.empty false |> Heap.insert ((h sx sy), sx, sy)
         
         let mutable found = None
 
-        let mutable visited = 0L
+        let mutable visited = 0
 
         while (Option.isNone found) && (not (Heap.isEmpty openSet)) do
             let ((_, cx,cy),nextOpenSet) = openSet |> Heap.uncons
 
             if cx = gx && cy = gy  then found <- Some (cx,cy) else
 
-            visited <- visited + 1L
-
+            visited <- visited + 1
             openSet <- nextOpenSet
+
             for (nx, ny) in (neighbors (cx,cy) (mx, my)) do
                 let tentative_gScore = gScore[cx,cy] + (w nx ny)
                 if tentative_gScore < gScore[nx,ny] then
@@ -58,11 +58,8 @@ module Day15 =
                     gScore[nx,ny] <- tentative_gScore
                     openSet <- openSet |> Heap.insert (tentative_gScore + nh, nx, ny) 
 
-        printfn "FINISH"
-
-        printfn "visited = %d" visited
         match found with
-        | Some c -> reconstruct_path cameFrom c
+        | Some c -> reconstructPath cameFrom c
         | None -> failwith "failure"    
 
     let run (text: string): string*string =
@@ -80,16 +77,9 @@ module Day15 =
             let vv = (v + r) - 1
             (vv % 9) + 1
 
+        let part1 =
+             astar (0,0) (mx-1, my-1) (mx,my) w |> List.tail |> List.map (fun (x,y) -> w x y) |> List.sum
+        let part2 =
+             astar (0,0) ((mx*5)-1, (my*5)-1) (mx*5,my*5) w |> List.tail |> List.map (fun (x,y) -> w x y) |> List.sum
 
-        let start = (0,0)
-        let goal = ((mx * 5)-1,(my*5)-1)
-
-        printfn "%A" goal
-
-        let path = astar start goal (mx*5,my*5) w |> List.tail
-
-        // for (x,y) in path do        
-        //     printfn "%A %A %A" (x,y) (x/100,y/100) (x%100,y%100)
-        let part1 = path |> List.map (fun (x,y) -> w x y) |> List.sum
-
-        (part1 |> string , "-1")
+        (part1|>string,part2|>string)
