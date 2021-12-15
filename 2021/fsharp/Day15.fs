5d520fba1b0e8300aef5d2d30a3ea3437f08ee27
Joseph Kingry <joe@kingry.ca>
2021-12-15 08:03:35 -0500

    2021 day 15 HEAPS!

--- 2021/fsharp/Day15.fs
+++ 2021/fsharp/Day15.fs
@@ -23,61 +23,55 @@ module Day15 =
         total_path
 
     open Checked 
-
-    type Node =
-        {
-            p: int*int
-            o: bool
-            index: int
-        }
+    open FSharpx.Collections
 
     let astar start goal (grid : int[,]) =
+        printfn "START"
         let (gx,gy) = goal
         let inline h (a,b) = (gx - a) + (gy - b)  
-
-        let mutable openSet = [start]
         let mutable cameFrom = Map.empty
         let (sx,sy) = start
         let gScore = Array2D.create (Array2D.length1 grid) (Array2D.length2 grid) System.Int32.MaxValue
         gScore[sx,sy] <- 0
 
-        let fScore = Array2D.create (Array2D.length1 grid) (Array2D.length2 grid) System.Int32.MaxValue
-        fScore[sx,sy] <- (h start)
+        //let fScore = Array2D.create (Array2D.length1 grid) (Array2D.length2 grid) System.Int32.MaxValue
+        let hs = (h start)
+        //fScore[sx,sy] <- hs
+        let mutable openSet = Heap.empty false |> Heap.insert (hs, sx, sy)
         
         let mutable found = None
-        let mutable mx = 0
-        let mutable my = 0
+        // let mutable mx = 0
+        // let mutable my = 0
 
         let mutable visited = 0L
 
-        while (Option.isNone found) && (not (List.isEmpty openSet)) do
-            let (cx,cy)::nextCurrent = openSet
+        while (Option.isNone found) && (not (Heap.isEmpty openSet)) do
+            let ((ch, cx,cy),nextOpenSet) = openSet |> Heap.uncons
 
             if cx = gx && cy = gy  then found <- Some (cx,cy) else
 
-            if cx > mx then
-                mx <- cx
-                printfn "mx %d" mx
-            if cy > my then
-                my <- cy
-                printfn "my %d" my
+            // if cx > mx then
+            //     mx <- cx
+            //     printfn "mx %d" mx
+            // if cy > my then
+            //     my <- cy
+            //     printfn "my %d" my
 
 
             // printfn "%A" current
             visited <- visited + 1L
 
-            openSet <- nextCurrent
+            openSet <- nextOpenSet
             for (nx, ny) in (neighbors (cx,cy) grid) do
                 let tentative_gScore = gScore[cx,cy] + grid.[nx, ny]
                 if tentative_gScore < gScore[nx,ny] then
                     cameFrom <- cameFrom |> Map.add (nx,ny) (cx,cy)
+                    let nh = h (nx,ny)
                     gScore[nx,ny] <- tentative_gScore
-                    fScore[nx,ny] <- tentative_gScore + (h (nx,ny))
-                    openSet <- (nx,ny)::openSet
-            openSet <- 
-                openSet
-                |> List.distinct
-                |> List.sortBy (fun (x,y) -> fScore[x,y])
+                    //fScore[nx,ny] <- tentative_gScore + nh
+                    openSet <- openSet |> Heap.insert (tentative_gScore + nh, nx, ny) 
+
+        printfn "FINISH"
 
         printfn "visited = %d" visited
         match found with
@@ -107,6 +101,8 @@ module Day15 =
         printfn "%A" goal
 
         let path = astar start goal grid |> List.tail
+        // for (x,y) in path do        
+        //     printfn "%A %A %A" (x,y) (x/100,y/100) (x%100,y%100)
         let part1 = path |> List.map (fun (x,y) -> grid[x,y]) |> List.sum
 
         (part1 |> string , "-1")
