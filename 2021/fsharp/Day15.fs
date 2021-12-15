1c88f4c9e1035118d655680f7b44f34b7ed304a4
Joseph Kingry <joe@kingry.ca>
2021-12-15 08:10:09 -0500

    2021 day 15 more arrays

--- 2021/fsharp/Day15.fs
+++ 2021/fsharp/Day15.fs
@@ -4,71 +4,59 @@ namespace AdventOfCode.FSharp.Y2021
 module Day15 =
     open AdventOfCode.FSharp.Util
 
-    let neighbors (x, y) grid =
-        [ 1, 0; -1, 0; 0, 1; 0, -1 ]
-        |> List.map (fun (dx, dy) -> dx + x, dy + y)
-        |> List.filter
-            (fun (nx, ny) ->
-                nx >= 0
-                && ny >= 0
-                && nx < Array2D.length1 grid
-                && ny < Array2D.length2 grid)
+    let neighbors (x, y) (mx,my) =
+        let mutable n = []
+        if x > 0 then n <- (x-1,y)::n
+        if y > 0 then n <- (x,y-1)::n
+        if x < (mx - 1) then n <- (x+1,y)::n
+        if y < (my - 1) then n <- (x,y+1)::n
+        n
 
-    let reconstruct_path cameFrom current =
-        let mutable total_path = [current]
-        let mutable c = current
-        while cameFrom |> Map.containsKey c do
-            c <- cameFrom.[c]
-            total_path <- c::total_path
+    let reconstruct_path (cameFrom: ((int*int) option)[,]) (cx,cy) =
+        let mutable total_path = [(cx,cy)]
+        let mutable x = cx
+        let mutable y = cy
+        while Option.isSome cameFrom.[x,y] do
+            let (nx,ny) = (cameFrom.[x,y]).Value
+            x <- nx
+            y <- ny
+            total_path <- (x,y)::total_path
         total_path
 
     open Checked 
     open FSharpx.Collections
 
-    let astar start goal (grid : int[,]) =
+    let astar (sx,sy) (gx,gy) (mx,my) (w: int->int->int) =
         printfn "START"
-        let (gx,gy) = goal
-        let inline h (a,b) = (gx - a) + (gy - b)  
-        let mutable cameFrom = Map.empty
-        let (sx,sy) = start
-        let gScore = Array2D.create (Array2D.length1 grid) (Array2D.length2 grid) System.Int32.MaxValue
+        let inline h x y = (gx - x) + (gy - y)
+        let cameFrom = Array2D.create mx my None
+
+        let gScore = Array2D.create mx my System.Int32.MaxValue
         gScore[sx,sy] <- 0
 
-        //let fScore = Array2D.create (Array2D.length1 grid) (Array2D.length2 grid) System.Int32.MaxValue
-        let hs = (h start)
-        //fScore[sx,sy] <- hs
+        let hs = (h sx sy)
+
         let mutable openSet = Heap.empty false |> Heap.insert (hs, sx, sy)
         
         let mutable found = None
-        // let mutable mx = 0
-        // let mutable my = 0
+
 
         let mutable visited = 0L
 
         while (Option.isNone found) && (not (Heap.isEmpty openSet)) do
-            let ((ch, cx,cy),nextOpenSet) = openSet |> Heap.uncons
+            let ((_, cx,cy),nextOpenSet) = openSet |> Heap.uncons
 
             if cx = gx && cy = gy  then found <- Some (cx,cy) else
 
-            // if cx > mx then
-            //     mx <- cx
-            //     printfn "mx %d" mx
-            // if cy > my then
-            //     my <- cy
-            //     printfn "my %d" my
-
-
-            // printfn "%A" current
             visited <- visited + 1L
 
             openSet <- nextOpenSet
-            for (nx, ny) in (neighbors (cx,cy) grid) do
-                let tentative_gScore = gScore[cx,cy] + grid.[nx, ny]
+            for (nx, ny) in (neighbors (cx,cy) (mx, my)) do
+                let tentative_gScore = gScore[cx,cy] + (w nx ny)
                 if tentative_gScore < gScore[nx,ny] then
-                    cameFrom <- cameFrom |> Map.add (nx,ny) (cx,cy)
-                    let nh = h (nx,ny)
+                    cameFrom[nx,ny] <- Some (cx,cy)
+                    let nh = h nx ny
                     gScore[nx,ny] <- tentative_gScore
-                    //fScore[nx,ny] <- tentative_gScore + nh
                     openSet <- openSet |> Heap.insert (tentative_gScore + nh, nx, ny) 
 
         printfn "FINISH"
@@ -84,25 +72,25 @@ module Day15 =
             |> splitLine
             |> Array.map (fun line -> line |> Seq.map (string >> int))
             |> array2D
+        let mx = grid |> Array2D.length1
+        let my = grid |> Array2D.length2
 
-        let expandGrid grid n = 
-            let dx = grid |> Array2D.length1
-            let dy = grid |> Array2D.length2
-            Array2D.init (dx*5) (dy*5) (fun x y -> 
-                let v = grid[x%dx, y%dy] 
-                let r = (x/dx) + (y/dy)
-                let vv = (v + r) - 1
-                (vv % 9) + 1)
-        let grid = expandGrid grid 4
+        let w x y =
+            let v = grid[x%mx, y%my] 
+            let r = (x/mx) + (y/my)
+            let vv = (v + r) - 1
+            (vv % 9) + 1
 
 
         let start = (0,0)
-        let goal = ((Array2D.length1 grid) - 1, (Array2D.length2 grid) - 1)
+        let goal = ((mx * 5)-1,(my*5)-1)
+
         printfn "%A" goal
 
-        let path = astar start goal grid |> List.tail
+        let path = astar start goal (mx*5,my*5) w |> List.tail
+
         // for (x,y) in path do        
         //     printfn "%A %A %A" (x,y) (x/100,y/100) (x%100,y%100)
-        let part1 = path |> List.map (fun (x,y) -> grid[x,y]) |> List.sum
+        let part1 = path |> List.map (fun (x,y) -> w x y) |> List.sum
 
         (part1 |> string , "-1")
