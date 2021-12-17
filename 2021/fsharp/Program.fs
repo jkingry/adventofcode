c64f2a415addc4e844a8aed9d60cf05f635268e8
Joseph Kingry <joe@kingry.ca>
2021-12-17 00:32:42 -0500

    2021 17 rough

--- 2021/fsharp/Program.fs
+++ 2021/fsharp/Program.fs
@@ -34,7 +34,8 @@ let days =
 """
       14, Day14.run, E 2447, Some "3018019237563"
       15, Day15.run, E 613, E 2899
-      16, Day16.run, None, None
+      16, Day16.run, E 901, Some "110434737925"
+      17, Day17.run, None, None
     ]
     |> List.map (fun (d, r, e1, e2) ->
             { day = d
