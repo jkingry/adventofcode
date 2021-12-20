86760383c645588a5b2ee57478fbba7fff5027aa
Joseph Kingry <joe@kingry.ca>
2021-12-19 21:52:34 -0500

    2021 dat 18 > 24 hours :(

--- 2020/fsharp/Program.fs
+++ 2020/fsharp/Program.fs
@@ -3,44 +3,47 @@ open AdventOfCode.FSharp.Y2020
 
 let inline E v = v |> string |> Some
 
+let inline old (part1: string -> string) (part2: string -> string) =
+  fun text output ->
+    part1 text |> output 1
+    part2 text |> output 2
+
 let days =
-    [ 1, 1, Day01.part1, E 32064
-      1, 2, Day01.part2, E 193598720
+    [ 
+      // 1, 1, Day01.part1, E 32064
+      // 1, 2, Day01.part2, E 193598720
 
-      2, 1, Day02.part1, None
-      2, 2, Day02.part2, None
+      // 2, 1, Day02.part1, None
+      // 2, 2, Day02.part2, None
 
-      3, 1, Day03.part1, None
-      3, 2, Day03.part2, None
+      // 3, 1, Day03.part1, None
+      // 3, 2, Day03.part2, None
 
-      17, 1, Day17.part1, None
-      17, 2, Day17.part2, None
+      // 17, 1, Day17.part1, None
+      // 17, 2, Day17.part2, None
 
-      18, 1, Day18.part1, None
-      18, 2, Day18.part2, None
+      // 18, 1, Day18.part1, None
+      // 18, 2, Day18.part2, None
 
-      19, 1, Day19.part1, None
-      19, 2, Day19.part2, None
+      // 19, 1, Day19.part1, None
+      // 19, 2, Day19.part2, None
 
-      20, 1, Day20.part1, None
-      20, 2, Day20.part2, None
+      // 20, 1, Day20.part1, None
+      // 20, 2, Day20.part2, None
 
-      21, 1, Day21.part1, None
-      21, 2, Day21.part2, None
+      // 21, 1, Day21.part1, None
+      // 21, 2, Day21.part2, None
 
-      22, 1, Day22.part1, None
-      22, 2, Day22.part2, None
+      // 22, 1, Day22.part1, None
+      // 22, 2, Day22.part2, None
 
-      23, 1, Day23.part1, None
-      23, 2, Day23.part2, None
+      // 23, 1, Day23.part1, None
+      // 23, 2, Day23.part2, None
 
-      24, 1, Day24.part1, None
-      24, 2, Day24.part2, None ]
-    |> List.map
-        (fun (d, p, r, e) ->
+      24, Day24.run, None, None ]
+    |> List.map (fun (d, r, e1, e2) ->
             { day = d
-              part = p
               run = r
-              expected = e })
+              expected = [ (1,e1); (2,e2) ] |> Map.ofList })
 
 runCommandLine days
--- 2020/fsharp/day24.fs
+++ 2020/fsharp/day24.fs
@@ -6,16 +6,85 @@ module Day24 =
     open System
     open System.Collections.Generic
 
-    let run input output =
-        let parseTile position line =
-            (1,2)
-        let createGrid (lobby, (count), (offsetX, offsetY)) (x,y) =
-            lobby, (count+1), (offsetX, offsetY)
+    type grid<'T> = ('T[,] * int)
 
-        let (lobby, count, _) =
+    module Grid =
+        let create size =
+            let grid = Array2D.zeroCreate size size
+            let offset = size / 2
+            (grid, offset)
+        let set (x: int) (y: int) (v: 'T) (grid: grid<'T>): grid<'T> =
+            let mutable (g,o) = grid            
+            let mutable (ax,ay) = (x + o, y + o)
+            let mutable sz = Array2D.length1 g
+            while ax < 0 || ay < 0 || ax >= sz || ay >= sz do                            
+                let sz' = sz * 2
+                let d = (sz' - sz)  / 2
+                g <- Array2D.init sz' sz' (fun x y -> if x >= d && y >= d && x < (sz + d) && y < (sz + d) then g[x-d,y-d] else Unchecked.defaultof<'T>)
+                o <- o + d
+                ax <- x + o
+                ay <- y + o
+                sz <- sz'
+                
+            g[ax,ay] <- v
+            g,o            
+        let get (x: int) (y: int) (grid: grid<'T>) =
+            let  (g,o) = grid            
+            let  (ax,ay) = (x + o, y + o)
+            let sz = Array2D.length1 g
+            if ax >= 0 && ay >= 0 && ax < sz && ay < sz then g[ax,ay] else Unchecked.defaultof<'T>
+
+        let setWith (x: int) (y: int) (getValue: 'T -> 'T) (grid: grid<'T>) =
+            set x y (get x y grid |> getValue) grid
+        let copy (g,o) =
+            (Array2D.copy g),o
+
+
+    let run input output =
+        let parseTile line =
+            let (q,r,_) = 
+                line |> Seq.fold (fun (q, r, s) c -> 
+                    match c with
+                    | 'n' -> (q,r-1,1) 
+                    | 's' -> (q,r+1,-1) 
+                    | 'e' -> ((q + if s >= 0 then 1 else 0),r,0) 
+                    | 'w' -> ((q - if s <= 0 then 1 else 0),r,0) 
+                    | a -> failwith (sprintf "Unreachable %A" a) ) (0,0,0)
+            q,r
+
+        let DARK_BIT = 0b1000y
+        let NEIGHBOR_MASK = 0b111y
+
+        let flip q r (g,c) =
+            let v = Grid.get q r g
+            let dark = (v &&& DARK_BIT) <> 0y
+            let neighbours = v &&& NEIGHBOR_MASK
+            let (v', c', delta) = 
+                if dark then (neighbours, c - 1, -1y) 
+                else (neighbours ||| DARK_BIT, c + 1, 1y)
+            
+            let g' = g |> Grid.set q r v'
+            let g' = 
+                [(1,0);(1,-1);(0,-1);(-1,0);(-1,1);(0,1)]
+                |> List.fold (fun g (dq,dr) -> g |> Grid.setWith (q+dq) (r+dr) (fun v -> v + delta)) g'
+            g',c'
+
+        let mutable (g,c) =
             input
             |> splitLine
-            |> Array.map (fun line -> line |> Seq.fold parseTile (0,0))
-            |> Array.fold createGrid ((Array2D.zeroCreate 10 10), 0, (0, 0))
+            |> Array.map parseTile
+            |> Array.fold (fun lobby (q,r) -> flip q r lobby) ((Grid.create 1000), 0)     
         
-        ()
+        c |> string |> output 1
+
+        let step ((g,o),c) =
+            let g' Array2D.copy g 
+            g |> Array2D.iteri (fun x y v -> 
+                (g',c') <- 
+                    match v with
+                    | 2 -> flip x y g'
+                    | 9 | 10 -> ()
+                    | v when (v &&& DARK_BIT) = DARK_BIT -> flip x y g'
+                    | _ -> ())
+
+
