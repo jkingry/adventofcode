040e7e741bcc15c4f158fa596cf31503a983002f
Joseph Kingry <joe@kingry.ca>
2021-12-21 22:00:17 -0500

    2021 day 19 faster

--- 2021/fsharp/Day19.fs
+++ 2021/fsharp/Day19.fs
@@ -25,17 +25,19 @@ module Day19 =
             |> Set.ofArray
 
         scan, beacons
+
+    let distanceMapAdd dmap pair =
+        let ((x,y,z),(a,b,c)) = pair
+        let distance = (x-a)*(x-a) + (y-b)*(y-b) + (z-c)*(z-c)
+        dmap |> Map.change distance (fun o -> 
+            match o with
+            | Some k -> Some (pair::k)
+            | None -> Some [pair])
     
-    let distanceMap (beacons: point3D seq) = 
+    let toDistanceMap (beacons: point3D seq) = 
         comb 2 (beacons |> Seq.toList)
         |> List.map list2tuple2
-        |> List.fold (fun dmap pair -> 
-            let ((x,y,z),(a,b,c)) = pair
-            let distance = (x-a)*(x-a) + (y-b)*(y-b) + (z-c)*(z-c)
-            dmap |> Map.change distance (fun o -> 
-                match o with
-                | Some k -> Some (pair::k)
-                | None -> Some [pair])) Map.empty
+        |> List.fold distanceMapAdd Map.empty
 
     let inline delta (a,b,c) (d,e,f) = (a-d,b-e,c-f)
 
@@ -60,7 +62,7 @@ module Day19 =
 
     let inline manhattan (a,b,c) (d,e,f) = abs(a-d)+abs(b-e)+abs(c-f)
 
-    let matchScans admap bdmap =                
+    let matchScans admap aud bdmap bud =                
         let tryFindTransform d1 d2 =
             let a1x,a1y = admap |> Map.find d1 |> List.exactlyOne
             let pb1x,pb1y = bdmap |> Map.find d1 |> List.exactlyOne
@@ -96,14 +98,8 @@ module Day19 =
                     //printfn "First distance failed %d - (%A->%A) vs (%A->%A)" d1 a1x a1y b1x b1y
                     None)
 
-        let uniquedistances m = 
-            m 
-            |> Map.toSeq
-            |> Seq.choose (fun (k,v) -> if v |> List.length = 1 then Some k else None)
-            |> Set.ofSeq
-
         let samedistances = 
-            Set.intersect (admap |> uniquedistances) (bdmap |> uniquedistances)
+            Set.intersect aud bud
             |> Seq.toList     
         //printfn " Same Distances %d" (samedistances |> List.length)     
 
@@ -120,7 +116,15 @@ module Day19 =
             |> Array.map parseScan
             |> Map.ofArray
         
-        let mutable distances = scans |> Map.map (fun _ v -> distanceMap v)
+        let mutable distances = scans |> Map.map (fun _ v -> toDistanceMap v)
+
+        let toUniqueDistances m = 
+            m 
+            |> Map.toSeq
+            |> Seq.choose (fun (k,v) -> if v |> List.length = 1 then Some k else None)
+            |> Set.ofSeq
+
+        let mutable uniquedistances = distances |> Map.map (fun _ v -> toUniqueDistances v)
 
         let mutable scanners = Map.empty |> Map.add 0 (0,0,0)
 
@@ -132,17 +136,30 @@ module Day19 =
                 |> comb 2 
                 |> List.map list2tuple2
                 |> List.pick (fun (an, bn) ->
-                    //printfn "try %d to %d" an bn
-                    match matchScans distances[an] distances[bn] with
+                    // printfn "try %d to %d" an bn
+                    match matchScans distances[an] uniquedistances[an] distances[bn] uniquedistances[bn] with
                     | Some (r,t) -> Some (an,bn,r,t)
                     | None -> None)
             
-            //printfn "  found R=%A T=%A" r t
+            // printfn "  found R=%A T=%A" r t
             
             let a_beacons = scans[an]
             let b_beacons = scans[bn]
             let b_beacons_transformed = b_beacons |> Set.map (rotate r >> translate t)
             let ab_beacons = a_beacons |> Set.union b_beacons_transformed
+            let ab_new = Set.difference ab_beacons a_beacons
+            // printfn "  - added %d points" (Set.count ab_new)
+            
+            let a_distmap = distances[an]
+            let ab_distmap =  
+                seq {
+                    let mutable n = 0
+                    for bp in ab_new do
+                        n <- n + 1
+                        for ap in (Seq.append ab_new a_beacons) |> Seq.skip n do
+                            yield (bp, ap)
+                }
+                |> Seq.fold distanceMapAdd a_distmap
 
             scanners <- scanners |> Map.add bn t
 
@@ -152,7 +169,11 @@ module Day19 =
                 |> Map.remove bn
             distances <-
                 distances
-                |> Map.add an (distanceMap ab_beacons)
+                |> Map.add an ab_distmap
+                |> Map.remove bn
+            uniquedistances <-
+                uniquedistances
+                |> Map.add an (toUniqueDistances ab_distmap)
                 |> Map.remove bn
             
         scans |> Map.values |> Seq.head |> Set.count |> string |> output 1
