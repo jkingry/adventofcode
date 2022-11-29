898881715c972c9f19bd72311ccc64058d9f9eb3
Joseph Kingry <jkingry@gmail.com>
2022-11-28 22:52:50 -0500

    day 21 done

--- 2021/fsharp/Day21.fs
+++ 2021/fsharp/Day21.fs
@@ -4,14 +4,10 @@ namespace AdventOfCode.FSharp.Y2021
 module Day21 =
     open AdventOfCode.FSharp.Util
 
-    let game (rolls,players) =
+    let runDeterministicRound (rolls,players) =
         let ((pos, score), next) = players
         let die = (rolls % 100) + 1
-        let dieTotal =
-            match die with
-            | 99 -> 200
-            | 100 -> 103
-            | _ -> (3*die) + 3
+        let dieTotal = (3*die) + 3 
 
         let newPos = (pos + dieTotal) % 10
         let newScore = score + (newPos+1)
@@ -19,17 +15,64 @@ module Day21 =
 
         newRolls, (next, (newPos, newScore))
 
-    let winValue (rolls,((_,p1score),(_,p2score))) =
+    let calculateDeterministicResult (rolls,((_,p1score),(_,p2score))) =
         if p1score >= 1000 then rolls * p2score |> Some
         elif p2score >= 1000 then rolls * p1score |> Some
         else None 
+
     
+
+
+    let rec runDiracGame (cache: Ref<Map<(int*int*int*int),(int64*int64)>>) gameState =
+        match cache.Value |> Map.tryFind gameState with
+        | Some (currentWins, otherWins) -> currentWins, otherWins
+        | None ->
+            let (currentPosition, currentScore, otherPosition, otherScore) = gameState
+
+            let mutable currentWins = 0L
+            let mutable otherWins = 0L
+            for roll1 = 1 to 3 do
+                for roll2 = 1 to 3 do
+                    for roll3 = 1 to 3 do
+                        let nextPosition = (currentPosition + roll1 + roll2 + roll3) % 10
+                        let nextScore = currentScore + (nextPosition+1)
+                        if nextScore >= 21 then
+                            currentWins <- currentWins + 1L
+                        else
+                            // keep playing
+                            let (universeCopyOtherWins, universeCopyCurrentWins) = runDiracGame cache (otherPosition, otherScore, nextPosition, nextScore)
+                            currentWins <- currentWins + universeCopyCurrentWins
+                            otherWins <- otherWins + universeCopyOtherWins
+            cache.Value <- Map.add gameState (currentWins, otherWins) cache.Value 
+            currentWins, otherWins
+
     let run (input: string) (output: int -> string -> unit) =
-        let mutable g = (0, ((1, 0), (0,0)))
+        let positions = 
+            input 
+            |> splitLine
+            |> Seq.map (splitSpace >> Array.last >> int)
+            |> List.ofSeq
+        
+        let p1 = positions.[0] - 1
+        let p2 = positions.[1] - 1
+
+        // part 1
+
+        let mutable gameState = (0, ((p1, 0), (p2,0)))
 
         let mutable w = None
         while Option.isNone w do
-            g <- game g
-            w <- winValue g
-        let (r,_) = g
-        w.Value |> string |> output 1 
+            gameState <- runDeterministicRound gameState
+            w <- calculateDeterministicResult gameState
+
+        w.Value |> string |> output 1
+
+        // part 2
+        let (w1, w2) = runDiracGame (ref Map.empty) (p1, 0, p2, 0)
+
+        max w1 w2 |> string |> output 2
+
+
+
+
+
