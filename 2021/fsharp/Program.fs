b1362c5039706139f7374ffcde595f80f3e0673d
Joseph Kingry <jkingry@gmail.com>
2022-11-28 22:52:50 -0500

    day 21 done

--- 2021/fsharp/Program.fs
+++ 2021/fsharp/Program.fs
@@ -37,7 +37,7 @@ let days =
       18, Day18.run, E 4243, E 4701
       19, Day19.run, E 315, E 13192
       20, Day20.run, E 5765, E 18509
-      21, Day21.run, E 797160, None
+      21, Day21.run, E 797160, Some "27464148626406"
       22, Day22.run, E 602574, Some "1288707160324706"
       23, Day23.run, None, None
     ]
