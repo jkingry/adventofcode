8960ec432aa5b70aace949618e83fedea0bb679e
Joseph Kingry <jkingry@gmail.com>
2022-12-02 06:59:56 -0500

    2022 day 2
    check if strategy is actually good

--- 2022/fsharp/Day02.fs
+++ 2022/fsharp/Day02.fs
@@ -24,15 +24,16 @@ module Day02 =
             |> string
             |> output 1      
 
-        values 
-            |> Seq.map(fun (them, result) -> 
-                1 + 
+        let part2 = 
+            values 
+                |> Seq.map(fun (them, result) ->  
                     match result with
-                    | 0 -> 0 + (them + 2) % 3  
-                    | 1 -> 3 + them
-                    | 2 -> 6 + (them + 1) % 3
+                    | 0 -> (1 + 0 + (them + 2) % 3, 1 + 6 + them)  
+                    | 1 -> (1 + 3 + them, 1 + 3 + them)
+                    | 2 -> (1 + 6 + (them + 1) % 3, 1 + 0 + them) 
                     | x -> failwithf "Unexpected: %i" x)
-            |> Seq.sum
-            |> string
-            |> output 2
+                |> Seq.fold (fun (a,b) (c,d) -> (a + c, b + d)) (0, 0)
+
+        printfn "You win by %i points" ((fst part2) - (snd part2))
+        part2 |> fst |> string |> output 2      
 
