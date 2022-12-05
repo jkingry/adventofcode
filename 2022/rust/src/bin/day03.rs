4bc312b2590cd2ae646bbf08b71e5ce3b4adf61a
Joseph Kingry <jkingry@gmail.com>
2022-12-05 18:52:01 -0500

    2022 rust - day 3 part 2

--- 2022/rust/src/bin/day03.rs
+++ 2022/rust/src/bin/day03.rs
@@ -1,11 +1,27 @@
 use std::{error::Error, io::{self}};
 use bstr::{io::BufReadExt};
 
-fn find_common(a: &[u8], b: &[u8]) -> u8 {
+fn find_common_2(a: &[u8], b: &[u8]) -> u8 {
     for item1 in a {
         for item2 in b {
             if item1 == item2 {
                 return item1.to_owned();
+            }
+        }
+    }
+
+    return 0;
+}
+
+fn find_common_3(a: &[u8], b: &[u8], c: &[u8]) -> u8 {
+    for item1 in a {
+        for item2 in b {
+            if item1 == item2 {
+                for item3 in c {
+                    if item1 == item3 {
+                        return item1.to_owned();
+                    }
+                }
             }
         }
     }
@@ -26,21 +42,49 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut part1 = 0;
 
+    let mut part2 = 0;
+
+    let mut group_count = 0;
+    let mut line1 = [0u8; 50]; 
+    let mut line1_len = 0;
+    let mut line2 = [0u8; 50]; 
+    let mut line2_len = 0;
+
     stdin.lock().for_byte_line(|line| {
         let mid = line.len() / 2;
 
         let sack1 = &line[..mid];
         let sack2 = &line[mid..];
         
-        let c = find_common(sack1, sack2);
+        let c = find_common_2(sack1, sack2);
 
         part1 += item_score(c);
+ 
+        match group_count {
+            0 => {
+                line1_len = line.len();
+                line1[..line1_len].copy_from_slice(line);
+            },
+            1 => {
+                line2_len = line.len();
+                line2[..line2_len].copy_from_slice(line);
+            },
+            2 => { 
+                let group_c = find_common_3(
+                    &line1[..line1_len], 
+                    &line2[..line2_len], 
+                    line);
+                part2 += item_score(group_c);
+            },
+            _ => panic!("wtf"),
+        };
+        group_count = (group_count + 1) % 3;
 
         Ok(true)
     })?;
     
     println!("Day 03 - Part 1: {}", part1);
-    println!("Day 03 - Part 2: {}", 0);
+    println!("Day 03 - Part 2: {}", part2);
 
     Ok(())
 }
