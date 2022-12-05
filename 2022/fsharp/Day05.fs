eae20e4890ebbb4e0658c984c0edc585eb660011
Joseph Kingry <jkingry@gmail.com>
2022-12-05 08:00:12 -0500

    2022 day 5 cleanup using maps

--- 2022/fsharp/Day05.fs
+++ 2022/fsharp/Day05.fs
@@ -6,53 +6,60 @@ module Day05 =
     open Checked
 
     let run (input: string) (output: int -> string -> unit) =    
-        let parts = input |> splitDoubleLine
-        let layoutLines = parts[0] |> splitLine
-        let lc = layoutLines |> Array.last |> ints |> Array.max
+        let parseLayoutLine stackCount stacks (line: string) =
+            let mutable s = stacks
+            for i in 1..stackCount do
+                let box = line[1 + ((i-1) * 4)]
+                if 'A' <= box && box <= 'Z' then 
+                    s <- s |> Map.change i (fun stack -> Some (box::(stack |> Option.defaultValue []))) 
+            s
 
-        let lists : list<char>[] = Array.create lc [] 
-        for q in layoutLines |> Array.take (layoutLines.Length - 1) do
-            for i in 0..(lc - 1) do
-                let c = q[1 + (i*4)]
-                if c >= 'A' && c <= 'Z' then
-                    lists[i] <- c::lists[i] 
-
-        let lists = lists|> Array.map List.rev 
-
-        printfn "%A" (List.head lists[0])
-
-        let parse line =
-            match line with
-            | Regex @"move (\d+) from (\d+) to (\d+)" [m; src; dest] ->
-                Some (int m, -1 + int src, -1 + int dest)
-            | _ -> None        
-        let b = 
-            parts[1] 
-            |> splitLine
-            |> Seq.choose parse
-
-        for (c, src, dest) in b do 
-            let mutable pick = []
-            for _ in 1..c do
-                let h::sl = lists[src]
-                lists[src] <- sl
-
-                pick <- h::pick
-            
-            for p in pick do
-                let dl = lists[dest]
-                lists[dest] <- p::dl
-
-        let part1 = lists |> Array.map (fun q -> List.head q) |> System.String
-
-
-        // let part1 = 
-        //     input
-        //     |> splitLine
-        //     |> Array.map(fun s -> 
-        //         1
-        //         )
-        //     |> Array.sum
+        let parseLayout layout =
+            let lines = layout |> splitLine
+            let stackCount = lines |> Array.last |> ints |> Array.max
+            lines 
+                |> Array.take (lines.Length - 1)
+                |> Array.fold (parseLayoutLine stackCount) Map.empty
+                |> Map.map (fun _ v -> List.rev v)
         
-        part1 |> output 1
-        0 |> string |> output 2
+        let parseInstructions instructions =
+            instructions
+            |> splitLine
+            |> Array.choose (fun line -> 
+                match line with 
+                | Regex @"move (\d+) from (\d+) to (\d+)" [amt; src; dest] -> Some (int amt, int src, int dest)
+                | _ -> None)
+                    
+        let (layout, instructions) = 
+            match input |> splitDoubleLine with
+            | [| a; b |] -> (parseLayout a, parseInstructions b)
+            | _ -> failwith "bad format"
+
+        let topBox layout = layout |> Map.toSeq |> Seq.sortBy fst |> Seq.map (snd >> List.head) |> Seq.toArray 
+
+        let CrateMover9000 (stacks: Map<int,List<char>>) (amt, src, dest) =
+            let mutable s = stacks
+            for _ in 1..amt do
+                s <- s 
+                    |> Map.add src (List.tail s[src]) 
+                    |> Map.add dest ((List.head s[src])::s[dest])
+            s  
+
+        let part1 = instructions |> Array.fold CrateMover9000 layout |> topBox
+
+        part1 |> System.String |> output 1
+
+        let CrateMover9001 (stacks: Map<int, List<char>>) (amt, src, dest) =
+            let mutable s = stacks
+
+            let mutable pick = []
+            for _ in 1..amt do
+                pick <- (List.head s[src])::pick
+                s <- s |> Map.add src (List.tail s[src]) 
+            for p in pick do
+                s <- s |> Map.add dest (p::s[dest])
+            s 
+
+        let part2 = instructions |> Array.fold CrateMover9001 layout |> topBox
+
+        part2 |> System.String |> output 2
