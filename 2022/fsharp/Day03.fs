f4f1c0591aaa5acc25982ce9c25ca5e93e4f3014
Joseph Kingry <jkingry@gmail.com>
2022-12-05 20:51:13 -0500

    2022 day 3 bit masks over sets

--- 2022/fsharp/Day03.fs
+++ 2022/fsharp/Day03.fs
@@ -5,9 +5,16 @@ module Day03 =
     open AdventOfCode.FSharp.Util
 
     let run (input: string) (output: int -> string -> unit) =    
-        
+        let toMask (s: char[]) =
+            s |> Array.fold (fun a c -> a ||| (1UL <<< ((int c) - (int 'A')))) 0UL
+
         let findCommonItem item =
-                item |> Seq.map Set.ofSeq |> Seq.reduce Set.intersect |> Seq.head
+            let bit =
+                 item 
+                |> Array.map toMask 
+                |> Array.reduce (fun a b -> a &&& b) 
+                |> System.Numerics.BitOperations.TrailingZeroCount
+            (int 'A') + bit |> char
 
         let charScore c =
             (int c) -
@@ -16,13 +23,13 @@ module Day03 =
         let lines = input |> splitLine
 
         // split each line in half
-        let part1 = lines |> Seq.map(fun s ->
-            let mid = s.Length / 2 
-            [s[..mid - 1]; s[mid..]])
+        let part1 = lines |> Array.map (fun s ->
+            let (a,b) = s.ToCharArray() |> Array.splitAt (s.Length / 2)
+            [| a; b |])
 
-        part1 |> Seq.map (findCommonItem >> charScore) |> Seq.sum |> string |> output 1
+        part1 |> Array.map (findCommonItem >> charScore) |> Seq.sum |> string |> output 1
 
         // group lines into sets of three
-        let part2 = lines |> Seq.chunkBySize 3
+        let part2 = lines |> Array.map (fun s -> s.ToCharArray()) |> Array.chunkBySize 3
 
-        part2 |> Seq.map (findCommonItem >> charScore) |> Seq.sum |> string |> output 2
+        part2 |> Array.map (findCommonItem >> charScore) |> Seq.sum |> string |> output 2
