7d85dce68fc976a3fa1ba897b77860e638a4c9fc
Joseph Kingry <jkingry@gmail.com>
2022-12-12 07:13:30 -0500

    2022 day 1
    use parseIntDelim

--- 2022/fsharp/Day01.fs
+++ 2022/fsharp/Day01.fs
@@ -4,7 +4,6 @@ namespace AdventOfCode.FSharp.Y2022
 module Day01 =
     open Checked
     open AdventOfCode.FSharp.Util
-    open System
 
     let run (input: byte array) (output: int -> string -> unit) =
         let elfs =
@@ -17,24 +16,12 @@ module Day01 =
         
         elfs |> Seq.sortDescending |> Seq.take 3 |> Seq.sum |> string |> output 2
 
-    let zero = byte '0' 
-    let newLine = byte '\n'
-
-    let inline parseInt (s: ReadOnlySpan<byte>) =
-        let mutable n = 0
-        for c in s do
-            n <- n * 10 + int (c - zero)  
-        n
-
-    let runFast input output =
-        let s = new ReadOnlySpan<byte>(input)
-
+    let runFast (input: byte array) output =
         let mutable sum = 0
-        let mutable p = 0
 
         let top3 = Array.zeroCreate 3
 
-        let handleElf sum =
+        let handleElf () =
             let mutable maxDiff = 0
             let mutable maxIndex = -1
             for j in 0..2 do
@@ -44,17 +31,21 @@ module Day01 =
                     maxDiff <- diff 
             if maxIndex >= 0 then
                 top3[maxIndex] <- sum
+            sum <- 0
 
-        for i in 0 .. s.Length - 1 do
-            if s[i] = newLine then
-                if i > p then
-                    let n = parseInt (s.Slice(p, i - p))
-                    sum <- sum + n
-                else
-                    handleElf sum
-                    sum <- 0
-                p <- i + 1
-        handleElf sum
+        let newLine = byte '\n'
+        let mutable i = 0
+        while i < (input.Length - 1) do 
+            if input[i] = newLine then
+                handleElf ()
+                i <- i + 1
+
+            let (ni, n) = parseIntToDelim input i newLine
+            sum <- sum + n
+
+            i <- ni
+
+        handleElf ()
 
         top3 |> Array.max |> string |> output 1
         (top3[0] + top3[1] + top3[2]) |> string |> output 2
