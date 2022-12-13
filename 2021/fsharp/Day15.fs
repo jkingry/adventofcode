c6a86eae223a843853ab928aad710f161227b220
Joseph Kingry <jkingry@gmail.com>
2022-12-12 22:58:01 -0500

    map common graph functions

--- 2021/fsharp/Day15.fs
+++ 2021/fsharp/Day15.fs
@@ -3,55 +3,6 @@ namespace AdventOfCode.FSharp.Y2021
 // Day 15: Chiton
 module Day15 =
     open AdventOfCode.FSharp.Util
-    open FSharpx.Collections
-
-    let neighbors (x, y) width height =
-        let mutable n = []
-        if x > 0 then n <- (x - 1, y) :: n
-        if y > 0 then n <- (x, y - 1) :: n
-
-        if x < (width - 1) then
-            n <- (x + 1, y) :: n
-
-        if y < (height - 1) then
-            n <- (x, y + 1) :: n
-        n
-
-    let dijkstra start goal width height (weight: int -> int -> int) =
-        let costs =
-            Array2D.create width height System.Int32.MaxValue
-
-        let visited = Array2D.create width height false
-
-        let mutable q =
-            Heap.empty false |> Heap.insert (0, start)
-
-        let mutable found = None
-
-        while Option.isNone found && not (Heap.isEmpty q) do
-            let ((cost, current), nq) = Heap.uncons q
-
-            if current = goal then found <- Some cost else
-            
-            q <- nq
-            let (x,y) = current
-
-            if visited.[x, y] then () else
-
-            visited.[x, y] <- true
-
-            for (nx, ny) in (neighbors current width height) do
-                if visited.[nx, ny] then () else
-
-                let newCost = cost + weight nx ny
-
-                if newCost < costs.[nx, ny] then
-                    costs.[nx, ny] <- newCost
-                    q <- q |> Heap.insert (newCost, (nx, ny))
-
-        match found with
-        | Some cost -> cost
-        | _ -> failwith "INFINITY"
 
     let run data output =
         let text = data |> text
@@ -66,19 +17,32 @@ module Day15 =
         let height = grid |> Array2D.length2
 
         let risk x y =
-            let baseRisk = grid.[x % width, y % height]
+            let baseRisk = grid[x % width, y % height]
             let tileFactor = (x / width) + (y / height)
             let risk = (baseRisk + tileFactor) - 1
             (risk % 9) + 1
 
-        let part1 =
-            dijkstra (0, 0) (width - 1, height - 1) width height risk
-        output 1 (part1 |> string)
+        let part1risk _ _ x y =
+            if x < 0 || y < 0 || x >= width || y >= height then None else
+            risk x y |> Some
 
-        let width = width * 5
-        let height = height * 5
+        let costs, q =
+            Dijkstra2D.init width height System.Int32.MaxValue
+            |> Dijkstra2D.add 0 0 0
+            |> Dijkstra2D.run 4 0 (OrthoGrid.movesToBuffers part1risk) (fun x y -> x = (width - 1) && y = (height - 1))
 
-        let part2 =
-            dijkstra (0, 0) (width - 1, height - 1) width height risk
-        output 2 (part2 |> string)
+        costs[width - 1, height - 1] |> string |> output 1
+
+        let width' = width * 5
+        let height' = height * 5
+
+        let part2risk _ _ x y =
+            if x < 0 || y < 0 || x >= width' || y >= height' then None else
+            risk x y |> Some
+
+        let costs, _ =
+            Dijkstra2D.init width' height' System.Int32.MaxValue
+            |> Dijkstra2D.add 0 0 0
+            |> Dijkstra2D.run 4 0 (OrthoGrid.movesToBuffers part2risk) (fun x y -> x = width' - 1 && y = height' - 1)
         
+        costs[width' - 1, height' - 1] |> string |> output 2
--- 2022/fsharp/Day12.fs
+++ 2022/fsharp/Day12.fs
@@ -6,34 +6,6 @@ module Day12 =
     open FSharpx.Collections
     open AdventOfCode.FSharp.Util 
 
-    let dijkstraInit starts =        
-        let gScore = starts |> Seq.fold (fun m n -> m |> Map.add n 0) Map.empty
-        let q = starts |> Seq.fold (fun q n -> q |> Heap.insert (0, n)) (Heap.empty false)
-
-        gScore, q
-
-    let dijkstraRun (gScore, q) moves goalFunc =        
-        let mutable q = q
-        let mutable gScore = gScore
-
-        let mutable found = None
-
-        while Option.isNone found && not (Heap.isEmpty q) do
-            let ((_, current), nq) = Heap.uncons q
-
-            if goalFunc current then
-                found <- gScore |> Map.find current |> Some 
-            else
-                q <- nq
-
-                for (move, moveCost) in moves current do
-                    let tentative_gScore = gScore[current] + moveCost
-
-                    if tentative_gScore < (gScore |> Map.tryFind move |> Option.defaultValue System.Int32.MaxValue) then
-                        gScore <- gScore |> Map.add move tentative_gScore
-                        q <- q |> Heap.insert (tentative_gScore, move)
-        gScore, q
-
     let run (input: byte array) (output: int -> string -> unit) =
         let lines = input |> text |> splitLine 
         let a = lines |> Array.map (
@@ -51,128 +23,73 @@ module Day12 =
         let mx = Array2D.length1 a
         let my = Array2D.length2 a
 
-        let canMoveUp (fx, fy) (tx, ty) =
+        let canMoveDown (tx, ty) (fx, fy) =
             let fh = a[fx, fy]
             let th = a[tx, ty]
             (fh + 1uy) >= th
 
-        let canMoveDown src dest = canMoveUp dest src
-
-        let moves allowFunc (x,y) =
-            seq {
-                if x > 0 && allowFunc (x,y) (x - 1,y) then yield ((x - 1,y), 1)
-                if y > 0 && allowFunc (x,y) (x,y - 1) then yield ((x,y - 1), 1)
-                if x < (mx - 1) && allowFunc (x,y) (x + 1,y) then yield ((x + 1,y), 1)
-                if y < (my - 1) && allowFunc (x,y) (x, y + 1) then yield ((x,y + 1), 1)
-            }
+        let moves (x, y) = 
+                (x, y) 
+                |> OrthoGrid.movesToSeq (mx, my)
+                |> Seq.filter (canMoveDown (x,y))
+                |> Seq.map (fun n -> (n, 1)) 
   
         let mutable firstA = None
-        let g = dijkstraInit [goal]
 
-        let (costs, _) = dijkstraRun g (moves canMoveDown) (fun (nx, ny) -> 
+        let stopSearching (nx, ny) = 
             if firstA.IsNone && a[nx, ny] = 0uy then firstA <- Some (nx, ny)
-            (nx, ny) = start)
+            (nx, ny) = start
+
+        let (costs, _) = 
+            DijkstraMap.init 0 [goal] 
+            |> DijkstraMap.run System.Int32.MaxValue moves stopSearching
         
         costs[start] |> string |> output 1
         costs[firstA.Value] |> string |> output 2
 
-    let moves2D mx my allowFunc cx cy (mxBuf: int array) (myBuf: int array) (mcBuf: int array) =
-        let mutable c = 0
-        if cx > 0 && allowFunc cx cy (cx - 1) cy then 
-            mxBuf[c] <- cx - 1
-            myBuf[c] <- cy
-            mcBuf[c] <- 1
-            c <- c + 1
-        if cy > 0 && allowFunc cx cy cx (cy - 1) then 
-            mxBuf[c] <- cx
-            myBuf[c] <- cy - 1
-            mcBuf[c] <- 1
-            c <- c + 1
-        if cx < (mx - 1) && allowFunc cx cy (cx + 1) cy then 
-            mxBuf[c] <- cx + 1
-            myBuf[c] <- cy
-            mcBuf[c] <- 1
-            c <- c + 1
-        if cy < (my - 1) && allowFunc cx cy cx (cy + 1) then 
-            mxBuf[c] <- cx
-            myBuf[c] <- cy + 1
-            mcBuf[c] <- 1                                                
-            c <- c + 1
-        c
 
-    let dijkstra2Dinit mx my starts =
-        let gScore = Array2D.create mx my System.Int32.MaxValue
-        let mutable q = Heap.empty false 
-        for (x,y) in starts do
-            gScore[x, y] <- 0
-            q <- q |> Heap.insert (0, (x, y))
-        gScore, q
-
-    let dijkstra2Drun (gScore, q) moveFunc goalFunc =
-        let mutable gScore = gScore
-        let mutable q = q
-
-        let mutable found = None
-
-        let mxBuf = Array.create 4 0
-        let myBuf = Array.create 4 0
-        let mcBuf = Array.create 4 0
-        
-        while Option.isNone found && not (Heap.isEmpty q) do
-            let ((_, (cx, cy)), nq) = Heap.uncons q
-
-            if goalFunc (cx, cy) then
-                found <- Some (Array2D.get gScore cx cy)
-            else
-                q <- nq
-
-                let moveCount = moveFunc cx cy mxBuf myBuf mcBuf
-                for i = 1 to moveCount do
-                    let mx = mxBuf[i - 1]
-                    let my = myBuf[i - 1]
-                    let moveCost = mcBuf[i - 1]
-
-                    let tentative_gScore = gScore[cx, cy] + moveCost
-
-                    if tentative_gScore < gScore[mx, my] then
-                        gScore[mx, my] <- tentative_gScore
-                        q <- q |> Heap.insert (tentative_gScore, (mx, my))
-        gScore, q
     
     let runFast (input: byte array) (output: int -> string -> unit) =
         let lines = input |> text |> splitLine 
         let a = lines |> Array.map (
             fun s -> s.ToCharArray()) |> array2D
 
-        let mutable start = (0,0)
-        let mutable goal = (0,0)
+        let mutable (sx, sy) = (0,0)
+        let mutable (gx, gy) = (0,0)
 
         let a = a |> Array2D.mapi (fun x y v -> 
             match v with
-            | 'S' -> start <- (x, y); 0uy
-            | 'E' -> goal <- (x,y); 25uy
+            | 'S' -> sx <- x; sy <- y; 0uy
+            | 'E' -> gx <- x; gy <- y; 25uy
             | _ -> (byte v) - (byte 'a'))
         
         let mx = Array2D.length1 a
         let my = Array2D.length2 a
 
-        let canMoveUp fx fy tx ty =
+        let canMoveDown tx ty fx fy =
             let fh = a[fx, fy]
             let th = a[tx, ty]
             (fh + 1uy) >= th
 
-        let canMoveDown fx fy tx ty = canMoveUp tx ty fx fy
+        let cost fx fy tx ty =
+            if OrthoGrid.checkBounds a tx ty && canMoveDown fx fy tx ty then
+                Some 1
+            else 
+                None
+
+        let moves = OrthoGrid.movesToBuffers cost
   
         let mutable firstA = None
-        let findStart (x,y) =
+        let findStart x y =
             if firstA.IsNone && a[x,y] = 0uy then
                 firstA <- Some (x, y)
-            start = (x,y)
+            (sx, sy) = (x,y)
 
-        let g = dijkstra2Dinit mx my [goal]
-        let (costs, _) = dijkstra2Drun g (moves2D mx my canMoveDown) findStart
-
-        let sx,sy = start
+        let (costs, _) = 
+            Dijkstra2D.init mx my System.Int32.MaxValue
+            |> Dijkstra2D.add gx gy 0
+            |> Dijkstra2D.run 4 0 moves findStart
+        
         costs[sx, sy] |> string |> output 1
 
         let fx,fy = firstA.Value
