9a6fdb5cb6c6d36b2db7a11522af44f9d9b3fe27
Joseph Kingry <jkingry@gmail.com>
2022-12-13 01:38:03 -0500

    2022 day 13 cleanup

--- 2022/fsharp/Day13.fs
+++ 2022/fsharp/Day13.fs
@@ -6,70 +6,73 @@ module Day13 =
     open AdventOfCode.FSharp.Util 
     open FParsec
 
-    type P =
-        | PList of P list
+    type Packet =
+        | PList of Packet list
         | PNum of int
+
+    let rec tostr p =
+        match p with
+        | PList items -> sprintf "[%s]" (String.concat "," (items |> List.map tostr))
+        | PNum n -> sprintf "%i" n    
+        
+    let rec compare a b =
+        match a,b with
+        | PNum(aval), PNum(bval) -> 
+            if aval = bval then None else Some (aval < bval)   
+        | PList(alist), PList(blist) -> 
+            match (alist,blist) with 
+            | x::xs,y::ys -> 
+                match compare x y with
+                | Some v -> Some v
+                | None -> compare (xs |> PList) (ys |> PList)
+            | [],[] -> None
+            | [],_ ->  Some true
+            | _,[] ->  Some false
+        | PNum(_), PList(_) -> compare ([a] |> PList) b
+        | PList(_), PNum(_) -> compare a ([b] |> PList)            
 
     let runParser p str =
         match run p str with
         | Success(result, _, _) -> result
-        | Failure(errorMsg, _, _) -> failwith (sprintf "Failure: %s" errorMsg)        
+        | Failure(errorMsg, _, _) -> failwithf "Failure: %s" errorMsg        
 
     let run (input: byte array) (output: int -> string -> unit) =
-        let fvalue, fvalueRef = createParserForwardedToRef<P, unit>()
+        let fvalue, fvalueRef = createParserForwardedToRef<Packet, unit>()
         let pnumber = pint32 |>> PNum
-        let rec plist = pstring "[" >>. sepBy fvalue (pstring ",") .>> pstring "]" |>> PList
+        let plist = pstring "[" >>. sepBy fvalue (pstring ",") .>> pstring "]" |>> PList
         fvalueRef.Value <- choice [pnumber; plist]        
 
+        let pairs = 
+            input 
+            |> text 
+            |> splitDoubleLine
+            |> Array.map (fun text ->
+                let parts = text |> splitLine |> Array.map (runParser fvalue)
+                parts[0],parts[1])
 
-        let pairs = input |> text |> splitDoubleLine
-
-        let x = None
-        let mutable t = 0
-
-        let rec tostr p =
-            match p with
-            | PList items -> sprintf "[%s]" (String.concat "," (items |> List.map tostr))
-            | PNum n -> sprintf "%i" n
-
-        let rec compare a b =
-            match a,b with
-            | PNum(aa), PNum(bb) -> 
-                if aa = bb then None else 
-                    Some (aa < bb)   
-            | PList(aa), PList(bb) -> 
-                match (aa,bb) with 
-                | xh::xr,bh::br -> 
-                    match compare xh bh with
-                    | Some v -> Some v
-                    | None -> compare (PList(xr)) (PList(br))
-                | [],[] -> None
-                | [],_ ->  Some true
-                | _,[] ->  Some false
-            | PNum(_), PList(_) -> compare (PList([a])) b
-            | PList(_), PNum(_) -> compare a (PList([b]))
+        let mutable part1 = 0
         
-        for (index, pair) in pairs |> Array.indexed do
-            let p = pair |> splitLine
-            let a = p[0] |> (runParser fvalue)
-            let b = p[1] |> (runParser fvalue)
-
+        for (index, (a,b)) in pairs |> Array.indexed do
             if (compare a b).Value then
-                t <- t + (index + 1)
+                part1 <- part1 + (index + 1)
 
-        let lines = 
-            ((input |> text) + "\n[[2]]\n[[6]]\n") 
-            |> splitDoubleLine 
-            |> Array.map splitLine 
+        part1 |> string |> output 1
+
+        let divPack1 = runParser fvalue "[[2]]"
+        let divPack2 = runParser fvalue "[[6]]"
+
+        let packets = 
+            pairs 
+            |> Array.map (fun (a,b) -> [|a;b|]) 
             |> Array.concat
+            |> Array.append [| divPack1; divPack2 |]
 
-        let foo = 
-            lines
-            |> Array.map (runParser fvalue)
-            |> Array.sortWith (fun a b -> if (compare a b).Value then -1 else 1 )
+        let packets = 
+            packets
+            |> Array.sortWith (fun a b -> if (compare a b).Value then -1 else 1)
             
-        let xx = foo |> Array.findIndex(fun q -> q = PList([PList([PNum(6)])]))
-        let yy = foo |> Array.findIndex (fun q -> q = PList([PList([PNum(2)])]))
+        let index1 = packets |> Array.findIndex(fun v -> v = divPack1)
+        let index2 = packets |> Array.findIndex (fun v -> v = divPack2)
 
-        t |> string |> output 1
-        ((xx + 1)*(yy + 1)) |> string |> output 2
+        let part2 = (index1 + 1) * (index2 + 1)
+        part2 |> string |> output 2
