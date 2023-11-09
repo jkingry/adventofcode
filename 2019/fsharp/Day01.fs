2f7c4ecc5a7ae5f8f351d3c4003012a70d33382b
Joe Kingry <joe@kingry.ca>
2023-11-08 19:30:50 -0500

    2019 compiled to new format

--- 2019/fsharp/Day01.fs
+++ 2019/fsharp/Day01.fs
@@ -1,16 +1,16 @@
 namespace AdventOfCode.FSharp.Y2019
 
-open System
-
 // The Tyranny of the Rocket Equation
-module Day1 =
+module Day01 =
+    open System
+    open AdventOfCode.FSharp.Util
 
-    let part1 (input: string seq) =
+    let part1 (input: string) =
         let f x = max ((x / 3) - 2) 0
-        let q = input |> Seq.map Int32.Parse |> Seq.map f
-        q |> Seq.sum
+        let q = input |> Seq.map int |> Seq.map f
+        q |> Seq.sum |> string
 
-    let part2 (input: string seq) =
+    let part2 (input: string) =
         let f x = max ((x / 3) - 2) 0
 
         let g x =
@@ -19,4 +19,4 @@ module Day1 =
 
         let h x = (List.unfold g x) |> List.sum
 
-        input |> Seq.map Int32.Parse |> Seq.map h |> Seq.sum
+        input |> splitLine |> Seq.map int |> Seq.map h |> Seq.sum |> string
--- 2019/fsharp/Program.fs
+++ 2019/fsharp/Program.fs
@@ -1,1 +1,16 @@
-﻿AdventOfCode.FSharp.Util.runCommandLine ()
+﻿open AdventOfCode.FSharp.NorthPole
+open AdventOfCode.FSharp.Y2019
+
+let inline E v = v |> string |> Some
+
+let inline old (part1: string -> string) (part2: string -> string) =
+    [ fun data output ->
+          let text = AdventOfCode.FSharp.Util.text data
+          part1 text |> output 1
+          part2 text |> output 2 ]
+
+let days =
+    [ 01, old Day01.part1 Day01.part2 ]
+    |> List.map (fun (d, r) -> { year = 2021; day = d; runs = r })
+
+runCommandLine days
