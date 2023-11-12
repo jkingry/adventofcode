4469fd9aeefc0cacc6282730e4ca5469e66ae16c
Joe Kingry <joe@kingry.ca>
2023-11-11 22:34:19 -0500

    2016 day 6

---
+++ 2016/fsharp/Day06.fs
@@ -0,0 +1,28 @@
+namespace AdventOfCode.FSharp.Y2016
+
+// Day 6
+module Day06 =
+    open AdventOfCode.FSharp.Util
+
+    let largestValueKey map =
+        map |> Map.toSeq |> Seq.maxBy snd |> fst
+
+    let smallestValueKey map =
+        map |> Map.toSeq |> Seq.minBy snd |> fst
+
+    let run (input: byte array) output =
+        let lines = input |> text |> splitLine
+
+        let msgLen = lines[0].Length
+
+        let frequencies =
+            lines
+            |> Array.fold
+                (fun maps line -> maps |> Array.mapi (fun i map -> map |> Counter.incr line[i]))
+                (Array.create msgLen Map.empty)
+
+        let part1 = frequencies |> Array.map largestValueKey
+        let part2 = frequencies |> Array.map smallestValueKey
+
+        System.String part1 |> output 1
+        System.String part2 |> output 2
