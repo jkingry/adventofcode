2eb14b182144bfe9a86e433b7ff8669cb357b1c4
Joe Kingry <joe@kingry.ca>
2023-11-16 22:01:12 -0500

    2016 day 12

---
+++ 2016/fsharp/Day12.fs
@@ -0,0 +1,72 @@
+namespace AdventOfCode.FSharp.Y2016
+
+// Day 12: Leonardo's Monorail
+module Day12 =
+    open AdventOfCode.FSharp.Util
+
+    type Instr =
+        | ISet of int * int
+        | ICpy of int * int
+        | IInc of int
+        | IDec of int
+        | ICmp of int * int
+        | IJmp of int
+        | INop
+
+    let run (input: byte array) (output: int -> string -> unit) =
+        let parseLine (line: string) =
+            let parts = line.Split(' ')
+
+            match parts[0] with
+            | "cpy" ->
+                let v = parts[1][0]
+
+                if '0' <= v && v <= '9' then
+                    ISet(parts[1] |> int, parts[2][0] - 'a' |> int)
+                else
+                    ICpy(v - 'a' |> int, parts[2][0] - 'a' |> int)
+            | "inc" -> IInc(parts[1][0] - 'a' |> int)
+            | "dec" -> IDec(parts[1][0] - 'a' |> int)
+            | "jnz" ->
+                let v = parts[1][0]
+
+                if '0' <= v && v <= '9' then
+                    let vn = parts[1] |> int
+                    if vn <> 0 then IJmp(parts[2] |> int) else INop
+                else
+                    ICmp(v - 'a' |> int, parts[2] |> int)
+            | _ -> failwithf "Invalid line: %s" line
+
+        let instructions = input |> text |> splitLine |> Array.map parseLine
+
+        let executeProgram (reg: int[]) =
+            let mutable ptr = 0
+
+            while ptr < instructions.Length do
+                let mutable jump = false
+
+                match instructions[ptr] with
+                | ICpy(r0, r1) -> reg[r1] <- reg[r0]
+                | ISet(v, r0) -> reg[r0] <- v
+                | IDec r0 -> reg[r0] <- reg[r0] - 1
+                | IInc r0 -> reg[r0] <- reg[r0] + 1
+                | ICmp(r0, v) ->
+                    if reg[r0] <> 0 then
+                        ptr <- ptr + v
+                        jump <- true
+                | IJmp(v) ->
+                    ptr <- ptr + v
+                    jump <- true
+                | INop -> ()
+
+                if not jump then
+                    ptr <- ptr + 1
+
+        let reg1 = Array.zeroCreate 4
+        executeProgram reg1
+        reg1[0] |> string |> output 1
+
+        let reg2 = Array.zeroCreate 4
+        reg2[2] <- 1
+        executeProgram reg2
+        reg2[0] |> string |> output 2
