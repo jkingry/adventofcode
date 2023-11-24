f60a4c33e5690418cf399e94df622cac7b471148
Joe Kingry <joe@kingry.ca>
2023-11-23 21:57:18 -0500

    2016 day 17 - fantomas

--- 2016/fsharp/Day17.fs
+++ 2016/fsharp/Day17.fs
@@ -13,29 +13,31 @@ module Day17 =
 
         let hasher = System.Security.Cryptography.MD5.Create()
         let encoding = System.Text.Encoding.UTF8
-        
+
         let moves passcode state =
-            if goal state then Seq.empty else
+            if goal state then
+                Seq.empty
+            else
 
-            let (x,y), path = state
+                let (x, y), path = state
 
-            let target: string = passcode + path
-            let targetBytes = encoding.GetBytes target
-            let hash = targetBytes |> hasher.ComputeHash
+                let target: string = passcode + path
+                let targetBytes = encoding.GetBytes target
+                let hash = targetBytes |> hasher.ComputeHash
 
-            seq {
-                if y > 0 && hash[0] >= 0xB0uy then
-                    yield ((x, y - 1), path + "U"), 1
+                seq {
+                    if y > 0 && hash[0] >= 0xB0uy then
+                        yield ((x, y - 1), path + "U"), 1
 
-                if y < 3 && (hash[0] % 16uy) >= 0x0Buy then
-                    yield ((x, y + 1), path + "D"), 1
+                    if y < 3 && (hash[0] % 16uy) >= 0x0Buy then
+                        yield ((x, y + 1), path + "D"), 1
 
-                if x > 0 && hash[1] >= 0xB0uy then
-                    yield ((x - 1, y), path + "L"), 1
+                    if x > 0 && hash[1] >= 0xB0uy then
+                        yield ((x - 1, y), path + "L"), 1
 
-                if x < 3 && (hash[1] % 16uy) >= 0x0Buy then
-                    yield ((x + 1, y), path + "R"), 1
-            }
+                    if x < 3 && (hash[1] % 16uy) >= 0x0Buy then
+                        yield ((x + 1, y), path + "R"), 1
+                }
 
         let infinite = System.Int32.MaxValue
         let passcode = input |> text |> splitLine |> Array.head
