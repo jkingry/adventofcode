698a22561441cb6dcf6e5dc8a03a2a2ae5b8f01e
Joe Kingry <joe@kingry.ca>
2023-11-24 22:47:46 -0500

    Bits module

--- 2016/fsharp/Day18.fs
+++ 2016/fsharp/Day18.fs
@@ -4,22 +4,7 @@ namespace AdventOfCode.FSharp.Y2016
 module Day18 =
     open AdventOfCode.FSharp.Util
 
-    type bits = System.Collections.BitArray
-
     let run (input: byte array) (output: int -> string -> unit) =
-        let toString (row: bits) =
-            let mutable buf = System.Text.StringBuilder row.Count
-
-            for i = 0 to row.Count - 1 do
-                buf <- buf.Append(if row[i] then '^' else '.')
-
-            buf.ToString()
-
-        let popCount (row: bits) =
-            let dest: int[] = Array.zeroCreate (((row.Count |> float) / 32.0) |> ceil |> int)
-            row.CopyTo(dest, 0)
-            dest |> Array.map (uint >> System.Numerics.BitOperations.PopCount) |> Array.sum
-
         let inputLine1 = input |> bsplit '\n'B |> Array.head
 
         let row = bits inputLine1.Length
@@ -38,13 +23,13 @@ module Day18 =
         let mutable total = 0
 
         for _ = 1 to 40 do
-            total <- total + (row.Count - popCount row)
+            total <- total + (row.Count - Bits.popCount row)
             nextRow row
 
         total |> string |> output 1
 
         for _ = 41 to 400000 do
-            total <- total + (row.Count - popCount row)
+            total <- total + (row.Count - Bits.popCount row)
             nextRow row
 
         total |> string |> output 2
