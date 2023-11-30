c20da342c1cc85a94e0792d73d4b567786a693f1
Joe Kingry <joe@kingry.ca>
2023-11-29 19:24:37 -0500

    reordering and expected test times

--- 2016/fsharp/Day07.fs
+++ 2016/fsharp/Day07.fs
@@ -5,6 +5,35 @@ module Day07 =
     open AdventOfCode.FSharp.Util
     open System.Text.RegularExpressions
 
+    let runRegex (input: byte array) output =
+        let lines = input |> text |> splitLine
+
+        let re = Regex "([a-z])(?!\\1)([a-z])\\2\\1"
+        let nre = Regex "\\[[a-z]*?([a-z])(?!\\1)([a-z])\\2\\1[a-z]*?\\]"
+        let ssl = Regex "(?<!\\[[a-z]*)([a-z])(?!\\1)([a-z])\\1.*\\[[a-z]*\\2\\1\\2"
+        let ssl2 = Regex "\\[[a-z]*([a-z])(?!\\1)([a-z])\\1.*(?<!\\[[a-z]*)\\2\\1\\2"
+
+        let isIp7 (line: string) =
+            (re.IsMatch line) && not (nre.IsMatch line)
+
+        lines
+        |> Array.fold
+            (fun count line ->
+                let x = if isIp7 line then 1 else 0
+                count + x)
+            0
+        |> string
+        |> output 1
+
+        lines
+        |> Array.fold
+            (fun count line ->
+                let x = if (ssl.IsMatch line) || (ssl2.IsMatch line) then 1 else 0
+                count + x)
+            0
+        |> string
+        |> output 2
+
     let runManual (input: byte array) output =
         let detect (line: byte[]) =
             let mutable p0 = 0uy
@@ -73,32 +102,3 @@ module Day07 =
 
         tls_count |> string |> output 1
         ssl_count |> string |> output 2
-
-    let runRegex (input: byte array) output =
-        let lines = input |> text |> splitLine
-
-        let re = Regex "([a-z])(?!\\1)([a-z])\\2\\1"
-        let nre = Regex "\\[[a-z]*?([a-z])(?!\\1)([a-z])\\2\\1[a-z]*?\\]"
-        let ssl = Regex "(?<!\\[[a-z]*)([a-z])(?!\\1)([a-z])\\1.*\\[[a-z]*\\2\\1\\2"
-        let ssl2 = Regex "\\[[a-z]*([a-z])(?!\\1)([a-z])\\1.*(?<!\\[[a-z]*)\\2\\1\\2"
-
-        let isIp7 (line: string) =
-            (re.IsMatch line) && not (nre.IsMatch line)
-
-        lines
-        |> Array.fold
-            (fun count line ->
-                let x = if isIp7 line then 1 else 0
-                count + x)
-            0
-        |> string
-        |> output 1
-
-        lines
-        |> Array.fold
-            (fun count line ->
-                let x = if (ssl.IsMatch line) || (ssl2.IsMatch line) then 1 else 0
-                count + x)
-            0
-        |> string
-        |> output 2
