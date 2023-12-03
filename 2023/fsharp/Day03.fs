9b45fc4e1411b411ff85d559f0cebfb81b7a44a0
Joe Kingry <joe@kingry.ca>
2023-12-03 15:05:08 -0500

    2023 day 3 part 2

--- 2023/fsharp/Day03.fs
+++ 2023/fsharp/Day03.fs
@@ -5,8 +5,9 @@ module Day03 =
     open AdventOfCode.FSharp.Util
 
     type Entry =
-        | Symbol of int * int
+        | Symbol of int * int * byte
         | Part of (int * int) * (int * int) * int
+        | Row
 
     let parse (input: byte[]) =
         seq {
@@ -20,7 +21,7 @@ module Day03 =
                     v <- (v * 10) + int (b - '0'B)
                 else
                     if b <> '.'B && b <> '\n'B then
-                        yield Symbol(row, col)
+                        yield Symbol(row, col, b)
 
                     if v > 0 then
                         let len = v |> float |> log10 |> ceil |> int
@@ -32,36 +33,101 @@ module Day03 =
                 if b = '\n'B then
                     row <- row + 1
                     col <- 0
+                    yield Row
                 else
                     col <- col + 1
 
         }
 
-    let run (input: byte[]) (output: int -> string -> unit) =
-        let isAdj symbol part =
-            match symbol, part with
-            | Symbol(x, y), Part((tx, ty), (bx, by), v) ->
-                if (tx - 1) <= x && x <= (bx + 1) && (ty - 1) <= y && y <= (by + 1) then
-                    Some v
-                else
-                    None
-            | _ -> None
+    let isAdj symbol part =
+        match symbol, part with
+        | Symbol(x, y, b), Part((tx, ty), (bx, by), v) ->
+            if (tx - 1) <= x && x <= (bx + 1) && (ty - 1) <= y && y <= (by + 1) then
+                Some(v, (x, y))
+            else
+                None
+        | _ -> None
 
-        let sumParts (total, (parts, symbols)) entry =
+    let matchPartsSymbols matchFunc values =
+        let parts = [| List.empty; List.empty |]
+        let symbols = [| List.empty; List.empty |]
+
+        for entry in values do
             match entry with
+            | Row ->
+                parts[0] <- parts[1]
+                parts[1] <- List.empty
+                symbols[0] <- symbols[1]
+                symbols[1] <- List.empty
             | Symbol _ ->
-                parts
-                |> List.fold
-                    (fun (t, (plist, s)) p ->
-                        match isAdj entry p with
-                        | Some v -> t + v, (plist, s)
-                        | None -> t, (p :: plist, s))
-                    (total, ([], entry :: symbols))
-            | Part _ ->
-                symbols
-                |> List.tryPick (fun s -> isAdj s entry)
+                parts[1]
+                |> List.tryHead
+                |> Option.bind (isAdj entry)
                 |> function
-                    | Some v -> total + v, (parts, symbols)
-                    | None -> total, (entry :: parts, symbols)
+                    | Some(v, b) ->
+                        if matchFunc b v then
+                            parts[1] <- parts[1] |> List.tail
+                    | None -> ()
 
-        input |> parse |> Seq.fold sumParts (0, ([], [])) |> fst |> string |> output 1
+                let mutable p0 = []
+
+                for p in parts[0] do
+                    match isAdj entry p with
+                    | Some(v, b) ->
+                        if matchFunc b v |> not then
+                            p0 <- p :: p0
+                    | None -> p0 <- p :: p0
+
+                symbols[1] <- entry :: symbols[1]
+            | Part _ ->
+                let s =
+                    symbols[1]
+                    |> List.tryHead
+                    |> function
+                        | Some s -> s :: symbols[0]
+                        | None -> symbols[0]
+
+                match s |> List.tryPick (fun s -> isAdj s entry) with
+                | Some(v, b) ->
+                    if matchFunc b v |> not then
+                        parts[1] <- entry :: parts[1]
+                | None -> parts[1] <- entry :: parts[1]
+
+    let run (input: byte[]) (output: int -> string -> unit) =
+        let parsed = input |> parse |> Seq.cache
+
+        let mutable partNumberTotal = 0
+
+        parsed
+        |> matchPartsSymbols (fun _ v ->
+            partNumberTotal <- partNumberTotal + v
+            true)
+
+        partNumberTotal |> string |> output 1
+
+        let gearsOnly =
+            parsed
+            |> Seq.filter (function
+                | Symbol(_, _, b) when b <> '*'B -> false
+                | _ -> true)
+
+        let mutable gears = Map.empty
+
+        gearsOnly
+        |> matchPartsSymbols (fun b v ->
+            gears <-
+                gears
+                |> Map.change b (function
+                    | Some s -> v :: s |> Some
+                    | None -> [ v ] |> Some)
+
+            false)
+
+        gears
+        |> Map.values
+        |> Seq.choose (function
+            | [ a; b ] -> a * b |> Some
+            | _ -> None)
+        |> Seq.sum
+        |> string
+        |> output 2
