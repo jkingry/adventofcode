9d51c3b4dfd150cb6a24d42de8e2699b698d4b10
Joe Kingry <joe@kingry.ca>
2023-12-02 22:08:04 -0500

    2015 day 1

---
+++ 2015/fsharp/Calendar.fs
@@ -0,0 +1,3 @@
+module AdventOfCode.FSharp.Y2015.Calendar
+
+let days = AdventOfCode.FSharp.NorthPole.findDays ()
---
+++ 2015/fsharp/Day01.fs
@@ -0,0 +1,24 @@
+namespace AdventOfCode.FSharp.Y2015
+
+// Day 1: Not Quite Lisp
+module Day01 =
+    open AdventOfCode.FSharp.Util
+
+    let run (input: byte array) (output: int -> string -> unit) =
+        let part1, part2 =
+            input
+            |> Array.indexed
+            |> Array.fold
+                (fun (floor, basementPosition) (i, b) ->
+                    let floor' = floor + if b = '('B then 1 else -1
+
+                    let basementPosition' =
+                        match basementPosition, floor' with
+                        | None, -1 -> i + 1 |> Some
+                        | _ -> basementPosition
+
+                    floor', basementPosition')
+                (0, None)
+
+        part1 |> string |> output 1
+        part2.Value |> string |> output 2
