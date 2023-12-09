10422474fac5e51428a066fa368749bb2b6595ae
Joe Kingry <joe@kingry.ca>
2023-12-08 23:08:34 -0500

    2015 day 3

--- 2015/fsharp/Day02.fs
+++ 2015/fsharp/Day02.fs
@@ -7,13 +7,20 @@ module Day02 =
     let run (input: byte array) (output: int -> string -> unit) =
         let mutable pos = 0
         let mutable total = 0
+        let mutable ribbon = 0
 
         while pos < input.Length do
             let p, a = parseIntToDelim input pos 'x'B
             let p, b = parseIntToDelim input p 'x'B
             let p, c = parseIntToAny input p
             pos <- p + 1
-            let s = [| a; b; c |] |> Array.sort
-            total <- total + (2 * a * b) + (2 * b * c) + (2 * c * a) + (s[0] * s[1])
+            let m = [ a; b; c ] |> List.sort
+            let m1 = m[0]
+            let m2 = m[1]
+
+            total <- total + (2 * a * b) + (2 * b * c) + (2 * c * a) + (m1 * m2)
+
+            ribbon <- ribbon + (2 * m1) + (2 * m2) + (a * b * c)
 
         total |> string |> output 1
+        ribbon |> string |> output 2
---
+++ 2015/fsharp/Day03.fs
@@ -0,0 +1,32 @@
+namespace AdventOfCode.FSharp.Y2015
+
+// Day 3: Perfectly Spherical Houses in a Vacuum
+module Day03 =
+    open AdventOfCode.FSharp.Util
+
+    let run (input: byte array) (output: int -> string -> unit) =
+        let mutable posYear1 = (0, 0)
+        let mutable year1 = Set.empty |> Set.add (0, 0)
+
+        let mutable year2 = year1
+        let mutable posYear2 = Array.create 2 (0, 0)
+        let mutable turn = 0
+
+        for b in input do
+            let move (x, y) =
+                match b with
+                | '>'B -> x + 1, y
+                | '<'B -> x - 1, y
+                | '^'B -> x, y + 1
+                | 'v'B -> x, y - 1
+                | _ -> failwithf "invalid: %c" (char b)
+
+            posYear1 <- posYear1 |> move
+            year1 <- year1 |> Set.add posYear1
+
+            posYear2[turn] <- posYear2[turn] |> move
+            year2 <- year2 |> Set.add posYear2[turn]
+            turn <- (turn + 1) % 2
+
+        year1.Count |> string |> output 1
+        year2.Count |> string |> output 2
