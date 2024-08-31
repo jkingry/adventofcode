4e1b772c3e88969eca9d56f5de13556dedf7ce25
Joseph Kingry <joe@kingry.ca>
2024-08-30 22:14:55 -0400

    2017 day 5 unsafe attempt
    For some reason Release mode is slower here, will have to look at IL, tried unsafe but didn't net any positive results

--- 2017/fsharp/Day05.fs
+++ 2017/fsharp/Day05.fs
@@ -8,12 +8,11 @@ module Day05 =
         let program = Array.copy p
         let mutable c = 0
         let mutable ip = 0
-        let programLen = program.Length
 
-        while ip >= 0 && ip < programLen do
-            let jmp = program[ip]
+        while ip >= 0 && ip < program.Length do
+            let jmp = program.[ip]
             let nip = ip + jmp
-            program[ip] <- jmpFun jmp
+            program.[ip] <- jmpFun jmp
             ip <- nip
             c <- c + 1
 
@@ -28,3 +27,32 @@ module Day05 =
         |> runProgram (fun jmp -> jmp + if jmp >= 3 then -1 else 1)
         |> string
         |> output 2
+
+
+// open FSharp.NativeInterop
+
+// let runProgramUnsafe jmpFun p =
+//     let program = p |> Array.copy
+//     let mutable c = 0
+//     let mutable ip = 0
+
+//     use buf = fixed &program[0]
+//     let mutable ptr = buf
+
+//     while ip >= 0 && ip < program.Length do
+//         let bjmp = ptr |> NativePtr.read
+//         let jmp = int bjmp
+
+//         (jmpFun bjmp) |> NativePtr.write ptr
+//         ptr <- jmp |> NativePtr.add ptr
+
+//         ip <- ip + jmp
+//         c <- c + 1
+//     c
+
+
+// let runUnsafe (input: byte array) (output: int -> string -> unit) =
+//     let mutable program = input |> parseInts
+
+//     program |> runProgramUnsafe (fun jmp -> jmp + 1) |> string |> output 1
+//     program |> runProgramUnsafe (fun jmp -> jmp + if jmp >= 3 then -1 else 1) |> string |> output 2
