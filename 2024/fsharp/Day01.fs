c714f4ec1248d106c3e71af2c9c26483378c4e14
Joseph Kingry <joe@kingry.ca>
2024-12-02 18:27:54 -0500

    2024 day 1 part 2

--- 2024/fsharp/Day01.fs
+++ 2024/fsharp/Day01.fs
@@ -1,9 +1,34 @@
 namespace AdventOfCode.FSharp.Y2024
 
-// Day 1: Not Quite Lisp https://adventofcode.com/2015/day/1
+// Day 1: Historian Hysteria
 module Day01 =
     open AdventOfCode.FSharp.Util
 
     let run (input: byte[]) (output: int -> string -> unit) =
-        output 1 "x"
-        output 2 "x"
+        let aGroup, bGroup =
+            input
+            |> bsplit '\n'B
+            |> Array.fold
+                (fun (aGroup, bGroup) line ->
+                    let (pos, aEntryId) = parseIntToAny line 0
+                    let (_, bEntryId) = parseIntToAny line pos
+                    aEntryId :: aGroup, bEntryId :: bGroup)
+                ([], [])
+
+        let aGroup = aGroup |> List.sort
+        let bGroup = bGroup |> List.sort
+
+        (aGroup, bGroup)
+        ||> List.zip
+        |> List.map (fun (aEntryId, bEntryId) -> aEntryId - bEntryId |> abs)
+        |> List.sum
+        |> string
+        |> output 1
+
+        aGroup 
+        |> List.map (fun aEntryId -> 
+            let countInOtherList = bGroup |> List.filter (fun bEntryId -> bEntryId = aEntryId) |> List.length
+            aEntryId * countInOtherList)
+        |> List.sum
+        |> string
+        |> output 2
