eaf38873fbe300f3c9df95765e442e363ca8f087
Joseph Kingry <joe@kingry.ca>
2024-12-04 04:54:22 -0500

    clean up

--- 2024/fsharp/Day04.fs
+++ 2024/fsharp/Day04.fs
@@ -13,18 +13,29 @@ module Day04 =
 
     let good = [ "SSMM"B; "MSSM"B; "SMMS"B; "MMSS"B ]
 
-    let readPos (grid: byte[][]) (r, c) =
+    let readPos (grid: byte[][]) pos =
+        let (r, c) = pos
+
         if r >= 0 && c >= 0 && r < grid.Length && c < grid[r].Length then
             Some(grid[r][c])
         else
             None
 
-    let search (grid: byte[][]) pos (dr, dc) =
-        Seq.unfold (fun (r, c) -> readPos grid (r, c) |> Option.map (fun next -> next, (r + dr, c + dc))) pos
-        |> Seq.truncate XMAS.Length
-        |> Seq.compareWith (fun a b -> int (a - b)) XMAS
+    let findXmas (grid: byte[][]) pos (dr, dc) =
+        let found =
+            pos
+            |> Seq.unfold (fun cur ->
+                let r, c = cur
+                let next = r + dr, c + dc
+                Some(cur, next))
+            |> Seq.map (readPos grid)
+            |> Seq.truncate XMAS.Length
+            |> Seq.choose id
+            |> Seq.compareWith (fun a b -> int (a - b)) XMAS
 
-    let search2 (grid: byte[][]) (r, c) =
+        if found = 0 then 1 else 0
+
+    let find2Mas (grid: byte[][]) (r, c) =
         let letters =
             diags
             |> Array.map (fun (dr, dc) -> r + dr, c + dc)
@@ -35,27 +46,18 @@ module Day04 =
     let run (input: byte array) (output: int -> string -> unit) =
         let lines = input |> bsplit '\n'B
 
-        let mutable found = 0
+        let mutable found1 = 0
+        let mutable found2 = 0
 
         for row = 0 to lines.Length - 1 do
             for col = 0 to lines[row].Length - 1 do
                 let c = lines[row][col]
 
                 if c = 'X'B then
-                    found <-
-                        dirs
-                        |> Array.map (search lines (row, col))
-                        |> Array.fold (fun a v -> a + if v = 0 then 1 else 0) found
-
-        found |> string |> output 1
-
-        let mutable found = 0
-
-        for row = 0 to lines.Length - 1 do
-            for col = 0 to lines[row].Length - 1 do
-                let c = lines[row][col]
+                    found1 <- found1 + (dirs |> Array.map (findXmas lines (row, col)) |> Array.sum)
 
                 if c = 'A'B then
-                    found <- found + search2 lines (row, col)
+                    found2 <- found2 + find2Mas lines (row, col)
 
-        found |> string |> output 2
+        found1 |> string |> output 1
+        found2 |> string |> output 2
