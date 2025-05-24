bac2ce54fc43fb6a2a4a430d8f8c953deff3789e
Joseph Kingry <joe@kingry.ca>
2025-05-24 15:36:39 -0400

    2019 day 1

--- 2019/fsharp/Day01.fs
+++ 2019/fsharp/Day01.fs
@@ -5,23 +5,25 @@ module Day01 =
     open System
     open AdventOfCode.FSharp.Util
 
-    let part1 (input: string) =
+    let part1 inputs =
         let f x = max ((x / 3) - 2) 0
-        let q = input |> Seq.map int |> Seq.map f
-        q |> Seq.sum |> string
+        let q = inputs |> Seq.map f
+        q |> Seq.sum
 
-    let part2 (input: string) =
+    let part2 inputs =
         let f x = max ((x / 3) - 2) 0
 
         let g x =
             let y = f x
             if y > 0 then Some(y, y) else None
 
-        let h x = (List.unfold g x) |> List.sum
+        let h x = 
+            List.unfold g x 
+            |> List.sum
 
-        input |> splitLine |> Seq.map int |> Seq.map h |> Seq.sum |> string
+        inputs |> Seq.map h |> Seq.sum
 
     let run (input: byte array) output =
-        let textInput = text input
-        part1 textInput |> output 1
-        part2 textInput |> output 2
+        let nums = parseInts input
+        part1 nums |> string |> output 1
+        part2 nums |> string |> output 2
