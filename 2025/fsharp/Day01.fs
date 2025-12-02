02b958015241999753fff050e9437258793a1536
Joseph Kingry <joe@kingry.ca>
2025-12-01 23:40:15 -0500

    2025 day 1

---
+++ 2025/fsharp/Day01.fs
@@ -0,0 +1,105 @@
+namespace AdventOfCode.FSharp.Y2025
+
+// Day 1
+module Day01 =
+    open AdventOfCode.FSharp.Util
+    open System
+
+    let inline rotateDial delta position =
+        let rawPosition = position + delta
+        let modPosition = rawPosition % 100
+        let newPosition = if modPosition < 0 then 100 + modPosition else modPosition
+
+        let zeroClicks =
+            if rawPosition > 0 then
+                rawPosition / 100
+            else
+                (rawPosition / 100 |> abs) + if position = 0 then 0 else 1
+
+        newPosition, zeroClicks
+
+    let inline parseLine (line: ReadOnlySpan<byte>) =
+        let numberSpan = line.Slice 1
+
+        parseIntSpan numberSpan
+        * match line[0] with
+          | 'R'B -> 1
+          | 'L'B -> -1
+          | _ -> failwith "invalid line"
+
+    let runSpan (input: byte array) (output: int -> string -> unit) =
+        let inputSpan = ReadOnlySpan<byte> input
+
+        let mutable zeroCount = 0
+        let mutable zeroClickCount = 0
+        let mutable position = 50
+
+        for r in inputSpan.Split '\n'B do
+            let lineSpan = trySlice r inputSpan
+
+            if lineSpan.Length > 0 then
+                let delta = parseLine lineSpan
+
+                let newPosition, clickCounts = rotateDial delta position
+
+                position <- newPosition
+                zeroClickCount <- zeroClickCount + clickCounts
+
+                if position = 0 then
+                    zeroCount <- zeroCount + 1
+
+        zeroCount |> string |> output 1
+        zeroClickCount |> string |> output 2
+
+    let run (input: byte array) (output: int -> string -> unit) =
+        input
+        |> bsplit '\n'B
+        |> Array.scan
+            (fun position line ->
+                let _, delta = parseIntToAny line 1
+                let delta = delta * if line[0] = 'L'B then -1 else 1
+                (position + delta) % 100)
+            50
+        |> Array.fold (fun total position -> total + if position = 0 then 1 else 0) 0
+        |> string
+        |> output 1
+
+    let runDumb (input: byte array) (output: int -> string -> unit) =
+        let deltas =
+            input
+            |> bsplit '\n'B
+            |> Array.map (fun line ->
+                let _, delta = parseIntToAny line 1
+                delta * if line[0] = 'L'B then -1 else 1)
+
+        let mutable position = 50
+        let mutable zeroCount = 0
+        let mutable zeroClicks = 0
+
+        for delta in deltas do
+            let mutable d = delta
+
+            if delta > 0 then
+                while d <> 0 do
+                    position <- (position + 1) % 100
+                    d <- d - 1
+
+                    if position = 0 then
+                        zeroClicks <- zeroClicks + 1
+            else
+                while d <> 0 do
+                    position <- (position - 1) % 100
+
+                    if position < 0 then
+                        position <- 100 + position
+
+                    d <- d + 1
+
+                    if position = 0 then
+                        zeroClicks <- zeroClicks + 1
+
+            if position = 0 then
+                zeroCount <- zeroCount + 1
+
+        zeroCount |> string |> output 1
+        zeroClicks |> string |> output 2
